09133b8561de367c1af82d0291ea636051cb3516
Jie Li <jieli@Jies-MacBook-Pro.local>
2019-09-09 11:29:11 -0500

    add get job list function

--- get_job_metrics.py
+++ get_job_metrics.py
@@ -7,21 +7,42 @@ def main():
 
     session = requests.Session()
 
-    job_info_list, err_info_list = get_hpcjob_data(conn_time_out, read_time_out, session)
+    # job_info_list, err_info_list = get_hpcjob_data(conn_time_out, read_time_out, session)
+    #
+    # if job_info_list != None:
+    #     # print(job_info_list)
+    #     with open("hpcjobs.json", "wb") as outfile:
+    #         json.dump(job_info_list, outfile, indent = 4)
+    #     print("Writing to file succeed")
+    # else:
+    #     print(err_info_list)
 
-    if job_info_list != None:
-        # print(job_info_list)
-        with open("hpcjobs.json", "wb") as outfile:
-            json.dump(job_info_list, outfile, indent = 4)
-        print("Writing to file succeed")
+    job_list, err_info = get_job_list(conn_time_out, read_time_out, session)
+
+    if job_list != None:
+        print(job_list)
     else:
-        print(err_info_list)
+        print(err_info)
+
+
+
+def get_job_list(conn_time_out, read_time_out, session):
+    try:
+        url = "http://129.118.104.35:8182/jobs"
+        response = session.get(url, verify = False, timeout = (conn_time_out, read_time_out))
+
+        response.raise_for_status()
+        data = response.json()
+
+        return data, str(None)
+    except requests.exceptions.RequestException as e:
+        print("Request Error")
+        return None, str(e)
 
 
 def get_hpcjob_data(conn_time_out, read_time_out, session):
     try:
         url = "http://129.118.104.35:8182/hostsummary/1/500"
-
         response = session.get(url, verify = False, timeout = (conn_time_out, read_time_out))
 
         response.raise_for_status()
