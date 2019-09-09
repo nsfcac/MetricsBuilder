4c436397725508d56bc60134d99d0c5baab762f9
Jie Li <jieli@Jies-MacBook-Pro.local>
2019-09-09 09:06:08 -0500

    add getJobmetrics.py

---
+++ getJobmetrics.py
@@ -0,0 +1,32 @@
+import json
+import requests
+
+def main():
+    conn_time_out = 15
+    read_time_out = 40
+
+    session = requests.Session()
+
+    job_info_list, err_info_list = get_hpcjob_data(conn_time_out, read_time_out, session)
+
+    if job_info_list != None:
+        print(job_info_list)
+    else:
+        print(err_info_list)
+
+
+def get_hpcjob_data(conn_time_out, read_time_out, session):
+    try:
+        url = "http://129.118.104.35:8182/hostsummary/1/500"
+
+        response = session.get(url, verify = False, timeout = (conn_time_out, read_time_out))
+
+        response.raise_for_status()
+        data = response.json()
+
+        return data, str(None)
+    except requests.exceptions.RequestException as e:
+        return None, str(e)
+
+if __name__ == "__main__":
+    main()
