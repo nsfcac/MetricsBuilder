ce608264cf13c36452451b96ddc6d454d573fc69
Jie Li <jieli@Jies-MacBook-Pro.local>
2019-09-09 11:38:07 -0500

    add get job list function

--- get_job_metrics.py
+++ get_job_metrics.py
@@ -22,8 +22,8 @@ def main():
     if job_list != None:
         # print(job_list)
         jobs_num = len(job_list)
-        print(jobs_num)
-        # print(f"Jobs numbers: {jobs_num}")
+        # print(jobs_num)
+        print(f"Jobs numbers: {jobs_num}")
     else:
         print(err_info)
 
