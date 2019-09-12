0dfffdaf1d6862100a2931d190bec06517edd08e
Jie Li <jieli@Jies-MacBook-Pro.local>
2019-09-12 16:59:06 -0500

    update

--- MetricsBuilder.py
+++ MetricsBuilder.py
@@ -17,25 +17,27 @@ def main():
 
     node_pwr_list = {}
 
-
-    print("Pulling Metrics From BMC...")
-
-    #######################################################
-    # Get exec hosts and fetch corresponding power usuage #
-    #######################################################
+    #######################
+    # Get exection hosts #
+    #######################
+    print("Getting Exection Host List...")
     exec_hosts, err_info = get_uge_info(conn_time_out, read_time_out, session, "exechosts")
-    if exec_hosts != None:
-        exechost_list = get_exechosts_ip(exec_hosts)
-        core_to_threads(exechost_list, node_pwr_list, conn_time_out, read_time_out, session)
-        # print(node_pwr_list)
-    else:
+
+    if exec_hosts == None:
         print("No Execution Host")
         return
+    else:
+        exechost_list = get_exechosts_ip(exec_hosts)
+
+        print("Pulling Metrics From BMC...")
+        core_to_threads(exechost_list, node_pwr_list, conn_time_out, read_time_out, session)
+        # print(node_pwr_list)
+
 
     #########################
     # Get current timestamp #
     #########################
-    timestamp = datetime.datetime.now().ctime()
+    time_stamp = datetime.datetime.now().ctime()
 
     print("Pulling Metrics From UGE...")
 
@@ -47,80 +49,29 @@ def main():
         print("Get Host Summary Error")
         return
 
+    ######################
+    # Interleave metrics #
+    ######################
     job_user_time_dic = match_job_user_time(job_set, host_summary)
 
-    job_pwr_list = calc_job_pwr(node_job_match, job_set, node_pwr_list, job_user_time_dic)
+    job_pwr_list = calc_job_pwr(node_job_match, job_set, node_pwr_list, job_user_time_dic, time_stamp)
 
-    # print(json.dumps(job_pwr_list, indent = 4, sort_keys = True))
-    # print(len(job_set))
+    ######################
+    # Write logs #
+    ######################
 
-    # uge_tasklist = ['jobs', 'hostsummary']
-    # uge_results = {'jobs': None, 'hostsummary': None}
-    # uge_errors = {'jobs': None, 'hostsummary': None}
-    #
-    # # For progress bar
-    # task_len = len(uge_tasklist)
-    # printProgressBar(0, task_len, prefix = 'Progress:', suffix = 'Complete', length = 50)
-    #
-    # # Get job list, host summary
-    # for index, task in enumerate(uge_tasklist):
-    #     uge_results[task], uge_errors[task] = get_uge_info(conn_time_out, read_time_out, session, task)
-    #     printProgressBar(index + 1, task_len, prefix = 'Progress:', suffix = 'Complete', length = 50)
-    #
-    # job_list = uge_results['jobs']
-    # host_summary = uge_results['hostsummary']
+    print("Writing log files...")
 
-    print("Interleaving Metrics ...")
-
-    # if job_list != None and host_summary != None:
-    #     job_set = get_job_set(job_list)
-    #     job_node_match = match_job_node(job_set, host_summary)
-    # else:
-    #     print(err_info)
-    #     return
-
-    # print("Job list length: ")，
-    # print(len(job_set))
-
-    # Get exec hosts power usage for each job
-    # for i, item in enumerate(job_node_match):
-    #     pwr_usage_tot = 0
-    #     pwr_usage = []
-    #     for exechost in item['ExecHosts']:
-    #         # print(node_pwr_list[exechost])
-    #         # if node_pwr_list[exechost] != None:
-    #         pwr_usage.append(node_pwr_list[exechost])
-    #         try:
-    #             pwr_usage_tot = pwr_usage_tot + node_pwr_list[exechost]
-    #         except TypeError:
-    #             pwr_usage_tot = None
-    #
-    #     item.update({'PowerConsumedWatts': pwr_usage, 'TotalPowerConsumedWatts': pwr_usage_tot, 'TimeStamp': timestamp})
-    #
-    # print("Writing log files...")
-
-    with open("./uge/JobPwr.json", "w") as outfile_jobpwr:
+    with open("./interleaved/JobNodePwr.json", "w") as outfile_jobpwr:
             json.dump(job_pwr_list, outfile_jobpwr, indent = 4, sort_keys = True)
 
-    with open("./uge/HostJob.json", "w") as outfile_hostjob:
-            json.dump(node_job_match, outfile_hostjob, indent = 4, sort_keys = True)
-    #
-    # with open("./uge/JobList.json", "w") as outfile_joblist:
-    #         json.dump(job_list, outfile_joblist, indent = 4, sort_keys = True)
-    #
-    # with open("./uge/JobSetSorted.json", "w") as outfile_jobset:
-    #         json.dump(sorted(job_set), outfile_jobset, indent = 4, sort_keys = True)
-    #
-    # with open("./uge/HostSummary.json", "w") as outfile_hostsum:
-    #         json.dump(host_summary, outfile_hostsum, indent = 4, sort_keys = True)
-    #
-    # with open("./interleaved/jobNodePower.json", "w") as outfile:
-    #         json.dump(job_node_match, outfile, indent = 4, sort_keys = True)
-    #
-    # print("Done!")
+    with open("./uge/HostSummary.json", "w") as outfile_hostsum:
+            json.dump(host_summary, outfile_hostsum, indent = 4, sort_keys = True)
 
-    # print(json.dumps(job_node_match, indent = 4, sort_keys = True))
+    with open("./uge/JobUserTime.json", "w") as outfile_jobusertime:
+            json.dump(job_user_time_dic, outfile_jobusertime, indent = 4, sort_keys = True)
 
+    print("Done!")
 
 # Get exec hosts list of ip addresses
 def get_exechosts_ip(exechosts):
@@ -152,6 +103,12 @@ def match_node_job(host_summary):
     return node_job_match, job_set
 
 def match_job_user_time(job_set, host_summary):
+
+    print("Preprocessing UGE Metrics ...")
+    # For progress bar
+    job_set_len = len(job_set)
+    printProgressBar(0, job_set_len, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
     job_user_time_dic = {}
     for index, jobId in enumerate(job_set):
         for host in host_summary:
@@ -163,14 +120,22 @@ def match_job_user_time(job_set, host_summary):
                     continue
             if flag == 1:
                 continue
+        printProgressBar(index + 1, job_set_len, prefix = 'Progress:', suffix = 'Complete', length = 50)
     return job_user_time_dic
 
 
 # Calculate PowerConsumedWatts for each Job
-def calc_job_pwr(node_job_match, job_set, node_pwr_list, job_user_time_dic):
+def calc_job_pwr(node_job_match, job_set, node_pwr_list, job_user_time_dic, time_stamp):
+
     job_pwr_list = []
+
+    print("Interleaving Power Metrics ...")
+    # For progress bar
+    job_set_len = len(job_set)
+    printProgressBar(0, job_set_len, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
     for index, job in enumerate(job_set):
-        job_pwr_dict = {'User': job_user_time_dic[job]['user'],'JobId': job, 'StartTime': job_user_time_dic[job]['startTime'],'ExecHosts':[], 'OccupationPct': [], 'PowerConsumedWatts': [], 'TotalPowerConsumedWatts': None}
+        job_pwr_dict = {'TimeStamp': time_stamp,'User': job_user_time_dic[job]['user'],'JobId': job, 'StartTime': job_user_time_dic[job]['startTime'],'ExecHosts':[], 'OccupationPct': [], 'PowerConsumedWatts': [], 'TotalPowerConsumedWatts': None}
         total_pwr = 0
         for node in node_job_match:
             for i in node['Counting']:
@@ -187,37 +152,9 @@ def calc_job_pwr(node_job_match, job_set, node_pwr_list, job_user_time_dic):
                     job_pwr_dict['PowerConsumedWatts'].append(pwr_each)
         job_pwr_dict.update({'TotalPowerConsumedWatts': round(total_pwr, 2)})
         job_pwr_list.append(job_pwr_dict)
+        printProgressBar(index + 1, job_set_len, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
     return job_pwr_list
-
-
-# # Build job-node matches
-# def match_job_node(job_set, host_summary):
-#
-#     # For progress bar
-#     job_set_len = len(job_set)
-#     printProgressBar(0, job_set_len, prefix = 'Progress:', suffix = 'Complete', length = 50)
-#
-#     job_node_match = []
-#     for index, jobId in enumerate(job_set):
-#         jobId_int = int(jobId)
-#         job_node_dict = {'JobId': jobId_int, 'User': None, 'StartTime': None, 'ExecHosts':[], 'LoadAvg':[], 'MemUsed': [], 'TotalMemUsed': None }
-#         mem_total = 0
-#         for host in host_summary:
-#             if host['jobList'] and jobId_int == host['jobList'][0]['id']:
-#                 job_node_dict.update({'User': host['jobList'][0]['user'], 'StartTime': host['jobList'][0]['startTime']})
-#                 host_ip = get_hostip(host['hostname'].split('.')[0])
-#                 mem_used = host['hostValues']['mem_used']
-#                 job_node_dict['ExecHosts'].append(host_ip)
-#                 job_node_dict['LoadAvg'].append(host['hostValues']['load_avg'])
-#                 job_node_dict['MemUsed'].append(mem_used)
-#                 mem_total = mem_total + float(mem_used.split('G')[0])
-#         if len(job_node_dict['ExecHosts']) != 0:
-#             job_node_match.append(job_node_dict)
-#         mem_total_str = str(round(mem_total,1)) + 'G'
-#         job_node_dict.update({'TotalMemUsed': mem_total_str})
-#         printProgressBar(index + 1, job_set_len, prefix = 'Progress:', suffix = 'Complete', length = 50)
-#     return job_node_match
 
 # Convert host name to ip address
 def get_hostip(hostname):
