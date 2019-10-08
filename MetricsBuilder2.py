3db8b8a700c1bc24d009606d61a6f5b9f57adb85
Jie Li <jieli@Jies-MacBook-Pro.local>
2019-10-08 15:53:05 -0500

    query influxdb

--- MetricsBuilder2.py
+++ MetricsBuilder2.py
@@ -9,33 +9,28 @@ def main():
         database='hpcc_monitoring_db')
 
     hostIp = '10.101.3.53'
-    measurement = "Node_Power_State"
     startTime = '2019-04-26T00:00:00Z'
     endTime = '2019-04-26T05:00:00Z'
+    measure_list = ["node_job_info", "Node_Power_State", "Node_Power_Usage", 
+                "Cluster_Nodes_Jobs_PWR_Usage", "BMC_Health", "Inlet_Health", 
+                "Node_Health", "CPU_Health", "Memory_Health", "Fan_Health",
+                "CPU_Usage", "Memory_Usage", "Fan_Speed", "CPU_Temperature", 
+                "Inlet_Temperature", "Node_LED_Indicator"]
 
-    result = query_db(client, hostIp, measurement, startTime, endTime)
-
-    for item in result:
-        print(item)
-    # print(json.dumps(result, indent=4))
+    for measurement in measure_list:
+        result = query_db(client, hostIp, measurement, startTime, endTime)
+        for item in result:
+            print(item)
 
 def query_db(client, hostIp, measurement, startTime, endTime):
     """Generate query string based on the ip address, 
     startTime and endTime(time range)
     SELECT * FROM measurement WHERE time >= *** AND time <= ***
-    measurement: 
-    node_job_info, Node_Power_State,
-    Node_Power_Usage, Cluster_Nodes_Jobs_PWR_Usage
-    BMC_Health, Inlet_Health, Node_Health, 
-    CPU_Health, Memory_Health, Fan_Health
-    CPU_Usage, Memory_Usage, Fan_Speed, 
-    CPU_Temperature, Inlet_Temperature
-    Node_LED_Indicator
     """
     query = ("SELECT * FROM " + measurement 
              + " WHERE host='" + hostIp 
              + "' AND time >= '" + startTime 
-             + "' AND time <= '" + endTime + "' LIMIT 5")
+             + "' AND time <= '" + endTime + "' LIMIT 1")
 
     # print(query)
     print("Querying data: " + measurement)
