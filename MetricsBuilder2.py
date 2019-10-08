188366ff4d4a6e15c43f7649fe8c91687b2af103
Jie Li <jieli@Jies-MacBook-Pro.local>
2019-10-08 16:23:06 -0500

    query influxdb

--- MetricsBuilder2.py
+++ MetricsBuilder2.py
@@ -11,11 +11,16 @@ def main():
     hostIp = '10.101.3.53'
     startTime = '2019-04-26T00:00:00Z'
     endTime = '2019-04-26T05:00:00Z'
-    measure_list = ["node_job_info", "Node_Power_State", "Node_Power_Usage", 
-                "Cluster_Nodes_Jobs_PWR_Usage", "BMC_Health", "Inlet_Health", 
-                "Node_Health", "CPU_Health", "Memory_Health", "Fan_Health",
-                "CPU_Usage", "Memory_Usage", "Fan_Speed", "CPU_Temperature", 
-                "Inlet_Temperature", "Node_LED_Indicator"]
+    # Another measurement jID(jobID)
+    measure_list = ["Current_Jobs_ID", "Current_Users", 
+                    "Job_Info", "node_job_info", 
+                    "system_metrics", "Cluster_Nodes_Jobs_PWR_Usage", 
+                    "Node_Power_Usage", "Node_Power_State", 
+                    "CPU_Usage", "Memory_Usage", 
+                    "Fan_Speed", "CPU_Temperature", "Inlet_Temperature", 
+                    "BMC_Health", "Inlet_Health", "Node_Health", "CPU_Health", 
+                    "Memory_Health", "Fan_Health", "Node_LED_Indicator", 
+                ]
 
     for measurement in measure_list:
         result = query_db(client, hostIp, measurement, startTime, endTime)
