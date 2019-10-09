5da70b8433ab2d68cb63a42371e53e834ef5567d
Jie Li <jieli@Jies-MacBook-Pro.local>
2019-10-09 16:42:18 -0500

    update measurement list

--- MetricsBuilder2.py
+++ MetricsBuilder2.py
@@ -11,15 +11,24 @@ def main():
     hostIp = '10.101.3.53'
     startTime = '2019-04-26T00:00:00Z'
     endTime = '2019-04-26T05:00:00Z'
-    measure_list = ["Current_Jobs_ID", "Current_Users", 
-                    "Job_Info", "node_job_info", 
-                    "system_metrics", "Cluster_Nodes_Jobs_PWR_Usage", 
-                    "Node_Power_Usage", "Node_Power_State", 
-                    "CPU_Usage", "Memory_Usage", 
-                    "Fan_Speed", "CPU_Temperature", "Inlet_Temperature", 
-                    "BMC_Health", "Inlet_Health", "Node_Health", "CPU_Health", 
-                    "Memory_Health", "Fan_Health", "Node_LED_Indicator", 
-                ]
+    measure_list = [
+        "Current_Jobs_ID", "Current_Users", 
+        "Job_Info", "node_job_info", 
+        "system_metrics", "Cluster_Nodes_Jobs_PWR_Usage", 
+        "Node_Power_Usage", "Node_Power_State", 
+        "CPU_Usage", "Memory_Usage", 
+        "Fan_Speed", "CPU_Temperature", "Inlet_Temperature", 
+        "BMC_Health", "Inlet_Health", "Node_Health", "CPU_Health", 
+        "Memory_Health", "Fan_Health", "Node_LED_Indicator"
+    ]
+
+    host_measure_list = [
+        "BMC_Health", "CPU_Health", "CPU_Temperature", 
+        "CPU_Usage", "Fan_Health", "Fan_Speed", "Inlet_Health",
+        "Inlet_Temperature", "Job_Info", "Memory_Health",
+        "Memory_Usage", "Node_Health", "Node_LED_Indicator",
+        "Node_Power_State", "Node_Power_Usage"
+    ]
 
     # for measurement in measure_list:
     #     result = query_db(client, hostIp, measurement, startTime, endTime)
@@ -36,10 +45,12 @@ def query_db(client, hostIp, measurement, startTime, endTime):
     startTime and endTime(time range)
     SELECT * FROM measurement WHERE time >= *** AND time <= ***
     """
-    query = ("SELECT * FROM " + measurement 
-             + " WHERE host='" + hostIp 
-             + "' AND time >= '" + startTime 
-             + "' AND time <= '" + endTime + "' LIMIT 1")
+    query = (
+        "SELECT * FROM " + measurement 
+        + " WHERE host='" + hostIp 
+        + "' AND time >= '" + startTime 
+        + "' AND time <= '" + endTime + "' LIMIT 1"
+    )
 
     # print(query)
     print("Querying data: " + measurement)
