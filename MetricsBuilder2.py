fe60b5bc7187abe88e657231eb1ba9e0af8191e5
artlands <lijie0416@gmail.com>
2019-10-15 14:05:20 -0500

    update

--- MetricsBuilder2.py
+++ MetricsBuilder2.py
@@ -40,28 +40,28 @@ def main():
 
     measure_uge_list = ["Job_Info"]
 
-    for item in measure_bmc_list:
-        metric = query_bmc(
-            client, hostIp, item, "MAX", startTime, endTime, timeInterval
-        )
-        outfile_name = "./influxdb/" + item + ".json"
-        with open(outfile_name, "w") as outfile:
-            json.dump(metric, outfile, indent = 4, sort_keys = True)
+    # for item in measure_bmc_list:
+    #     metric = query_bmc(
+    #         client, hostIp, item, "MAX", startTime, endTime, timeInterval
+    #     )
+    #     outfile_name = "./influxdb/" + item + ".json"
+    #     with open(outfile_name, "w") as outfile:
+    #         json.dump(metric, outfile, indent = 4, sort_keys = True)
 
     # query = """SELECT DISTINCT("job_data") as "job_data" FROM Job_Info WHERE host='10.101.3.53'AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z' GROUP BY *, time(5m) SLIMIT 1"""
     # result = list(client.query(query).get_points())
     # with open("./influxdb/dist_sample.json", "w") as outfile:
     #     json.dump(result, outfile, indent = 4, sort_keys = True)
 
-    # query = """SELECT MAX("FAN_1") as "FAN_1","FAN_2","FAN_3","FAN_4" FROM Fan_Speed WHERE host='10.101.3.53' AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z'' GROUP BY *, time(5m) SLIMIT 1"""
-    # result = list(client.query(query).get_points())
-    # with open("./influxdb/sample.json", "a") as outfile:
-    #     json.dump(result, outfile, indent = 4, sort_keys = True)
+    query = """SELECT MAX("FAN_1") as "FAN_1","FAN_2","FAN_3","FAN_4" FROM Fan_Speed WHERE host='10.101.3.53' AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z'' GROUP BY *, time(5m) SLIMIT 1"""
+    result = list(client.query(query).get_points())
+    with open("./influxdb/sample.json", "a") as outfile:
+        json.dump(result, outfile, indent = 4, sort_keys = True)
 
     # SELECT MAX("CPU1 Temp") as "CPU1 Temp","CPU2 Temp" FROM CPU_Temperature WHERE host='10.101.3.53' AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z'' GROUP BY *, time(5m) SLIMIT 1
-    # SELECT MAX("Inlet Temp")FROM Inlet_Temperature WHERE host='10.101.3.53' AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z'' GROUP BY *, time(5m) SLIMIT 1
-    # SELECT MAX("cpuusage")FROM CPU_Usage WHERE host='10.101.3.53' AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z'' GROUP BY *, time(5m) SLIMIT 1
-    # SELECT MAX("memoryusage")FROM Memory_Usage WHERE host='10.101.3.53' AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z'' GROUP BY *, time(5m) SLIMIT 1
+    # SELECT MAX("Inlet Temp") FROM Inlet_Temperature WHERE host='10.101.3.53' AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z'' GROUP BY *, time(5m) SLIMIT 1
+    # SELECT MAX("cpuusage") FROM CPU_Usage WHERE host='10.101.3.53' AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z'' GROUP BY *, time(5m) SLIMIT 1
+    # SELECT MAX("memoryusage") FROM Memory_Usage WHERE host='10.101.3.53' AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z'' GROUP BY *, time(5m) SLIMIT 1
     # SELECT MAX("FAN_1") as "FAN_1","FAN_2","FAN_3","FAN_4" FROM Fan_Speed WHERE host='10.101.3.53' AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z'' GROUP BY *, time(5m) SLIMIT 1
 
 def query_bmc(client, hostIp, measurement, measureType, startTime, endTime, timeInterval):
