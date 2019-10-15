f60f98d0e9949efb50b0cec401b38081d47b97d7
artlands <lijie0416@gmail.com>
2019-10-15 13:34:55 -0500

    update

--- MetricsBuilder2.py
+++ MetricsBuilder2.py
@@ -31,38 +31,39 @@ def main():
     #     "Node_Power_State", "Node_Power_Usage"
     # ]
 
-    measure_list_viz = [
+    measure_bmc_list = [
         "CPU_Temperature", "Inlet_Temperature", 
         "CPU_Usage", "Memory_Usage",
         "Fan_Speed", "Node_Power_Usage",
-        "Job_Info"
+        
     ]
+    measure_uge_list = ["Job_Info"]
 
-    # for item in measure_list_viz:
-    #     metric = query_db(
-    #         client, hostIp, item, startTime, endTime, timeInterval
-    #     )
-    #     outfile_name = "./influxdb/" + item + ".json"
-    #     with open(outfile_name, "w") as outfile:
-    #         json.dump(metric, outfile, indent = 4, sort_keys = True)
+    for item in measure_bmc_list:
+        metric = query_bmc(
+            client, hostIp, item, "MAX", startTime, endTime, timeInterval
+        )
+        outfile_name = "./influxdb/" + item + ".json"
+        with open(outfile_name, "w") as outfile:
+            json.dump(metric, outfile, indent = 4, sort_keys = True)
 
-    query = """SELECT DISTINCT("job_data") as "job_data" FROM Job_Info WHERE host='10.101.3.53'AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z' GROUP BY *, time(5m) SLIMIT 1"""
-    result = list(client.query(query).get_points())
-    with open("./influxdb/dist_sample.json", "w") as outfile:
-        json.dump(result, outfile, indent = 4, sort_keys = True)
+    # query = """SELECT DISTINCT("job_data") as "job_data" FROM Job_Info WHERE host='10.101.3.53'AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z' GROUP BY *, time(5m) SLIMIT 1"""
+    # result = list(client.query(query).get_points())
+    # with open("./influxdb/dist_sample.json", "w") as outfile:
+    #     json.dump(result, outfile, indent = 4, sort_keys = True)
 
     # query = "SELECT * FROM Inlet_Temperature WHERE host='10.101.3.53'AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z' LIMIT 1"
     # result = list(client.query(query).get_points())
     # with open("./influxdb/sample.json", "a") as outfile:
     #     json.dump(result, outfile, indent = 4, sort_keys = True)
 
-def query_db(client, hostIp, measurement, startTime, endTime, timeInterval):
+def query_bmc(client, hostIp, measurement, measureType, startTime, endTime, timeInterval):
     """Generate query string based on the ip address, 
     startTime and endTime(time range)
     SELECT * FROM measurement WHERE time >= *** AND time <= ***
     """
     if measurement == "CPU_Temperature":
-        select_obj = """ ("CPU1 Temp") as "CPU1 Temp","CPU2 Temp" """
+        select_obj = """("CPU1 Temp") as "CPU1 Temp","CPU2 Temp" """
     elif measurement == "Inlet_Temperature":
         select_obj = "Inlet Temp"
     elif measurement == "CPU_Usage":
@@ -70,20 +71,17 @@ def query_db(client, hostIp, measurement, startTime, endTime, timeInterval):
     elif measurement == "Memory_Usage":
         select_obj = "memoryusage"
     elif measurement == "Fan_Speed":
-        select_obj = """ ("FAN_1") as "FAN_1","FAN_2","FAN_3","FAN_4" """
-    elif measurement == "Node_Power_Usage":
-        select_obj = "powerusage_watts"
+        select_obj = """("FAN_1") as "FAN_1","FAN_2","FAN_3","FAN_4" """
     else:
-        select_obj = "job_data"
+        select_obj = "powerusage_watts"
 
     query = (
-        "SELECT * "
-        # + tag_name
+        "SELECT " + measureType + select_obj
         + "FROM " + measurement 
         + " WHERE host='" + hostIp 
         + "' AND time >= '" + startTime 
         + "' AND time <= '" + endTime + "'"
-        # + "' GROUP BY time(" + timeInterval + "),* SLIMIT 1"
+        + "' GROUP BY *, time(" + timeInterval + ") SLIMIT 1"
     )
 
     # print(query)
