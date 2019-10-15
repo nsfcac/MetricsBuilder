917849306e832f4eaeeaf3bdca0240987a45c5cc
artlands <lijie0416@gmail.com>
2019-10-15 10:43:37 -0500

    update

--- MetricsBuilder2.py
+++ MetricsBuilder2.py
@@ -46,12 +46,12 @@ def main():
     #     with open(outfile_name, "w") as outfile:
     #         json.dump(metric, outfile, indent = 4, sort_keys = True)
 
-    query = "SELECT MAX('CPU1 Temp') as 'CPU1 Temp','CPU2 Temp','host','error' FROM CPU_Temperature WHERE host='10.101.3.53'AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z' GROUP BY time(5m) LIMIT 1"
+    query = "SELECT MAX('CPU1 Temp') as 'CPU1 Temp','CPU2 Temp','host','error' FROM 'CPU_Temperature' WHERE host='10.101.3.53'AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z' GROUP BY time(5m) LIMIT 1"
     result = list(client.query(query).get_points())
     with open("./influxdb/sample.json", "a") as outfile:
         json.dump(result, outfile, indent = 4, sort_keys = True)
 
-    query = "SELECT MAX('Inlet Temp') as 'Inlet Temp', 'host', 'error' FROM Inlet_Temperature WHERE host='10.101.3.53'AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z' GROUP BY time(5m),* SLIMIT 1"
+    query = "SELECT MAX('Inlet Temp') as 'Inlet Temp', 'host', 'error' FROM 'Inlet_Temperature' WHERE host='10.101.3.53'AND time >= '2019-04-26T00:00:00Z' AND time <= '2019-04-26T05:00:00Z' GROUP BY time(5m),* SLIMIT 1"
     result = list(client.query(query).get_points())
     with open("./influxdb/sample.json", "a") as outfile:
         json.dump(result, outfile, indent = 4, sort_keys = True)
