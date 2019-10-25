795aa7d98322e451523dfac3ec27b65bda381835
artlands <lijie0416@gmail.com>
2019-10-25 13:57:16 -0500

    collecting data

--- MetricsBuilder2.py
+++ MetricsBuilder2.py
@@ -420,8 +420,8 @@ def api_filter():
     print("Return aggregated metrics!")
     print("---%s seconds---" % (time.time() - start_time))
     return jsonify(returnData)
-    # outfile_name = "./influxdb/returnData.json"
-    # with open(outfile_name, "w") as outfile:
-    #     json.dump(returnData, outfile, indent = 4, sort_keys = True)
+    outfile_name = "./influxdb/returnData.json"
+    with open(outfile_name, "w") as outfile:
+        json.dump(returnData, outfile, indent = 4, sort_keys = True)
 
 app.run(host='0.0.0.0')
