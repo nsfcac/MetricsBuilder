e9f8e305cf04e984ffc6d01882cb93d8af3b9d20
artlands <lijie0416@gmail.com>
2019-11-05 17:20:31 -0600

    add getinfluxdbtable

---
+++ GetInfluxdbTable.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python3
+import json
+from influxdb import InfluxDBClient
+from MetricsBuilder3_QAtime import query_bmc, query_uge
+
+def main():
+    # Initialization
+    startTime = "2019-04-20T00:00:00Z"
+    endTime = "2019-04-20T01:00:00Z"
+    timeInterval = "5m"
+    valueType = "MEAN"
+    hostIp = "10.101.1.1"
+    client = InfluxDBClient(
+        host='localhost', 
+        port=8086, 
+        database='hpcc_monitoring_db'
+    )
+
+    measure_bmc_list = [
+        "CPU_Temperature", "Inlet_Temperature", 
+        "CPU_Usage", "Memory_Usage",
+        "Fan_Speed", "Node_Power_Usage",
+    ]
+    # measure_uge_list = ["Job_Info"]
+
+    print("Writing result into file...")
+    with open("./influxdb/table.json") as outfile:
+        # Query BMC metrics
+        for m in measure_bmc_list:
+            print(f"Get metric: {m} from BMC...\n")
+            bmc_metric = query_bmc(
+                client, hostIp, m, valueType, startTime, endTime, timeInterval
+            )
+            json.dump(bmc_metric[0], outfile)
+        # Query UGE metrics
+        print(f"Get metric: Job_Info from UGE...\n")
+        uge_metric = query_uge(
+            client, hostIp, startTime, endTime, timeInterval
+        )
+        json.dump(uge_metric[0], outfile)
+    print("Done!")
+            
+if __name__ == "__main__":
+    main()
