6de688a531cbd140b5c28f336add5c1160d6a5c6
artlands <lijie0416@gmail.com>
2019-11-06 10:08:25 -0600

    update Get

--- GetInfluxdbTable.py
+++ GetInfluxdbTable.py
@@ -6,10 +6,10 @@ from MetricsBuilder3_QAtime import query_bmc, query_uge
 def main():
     # Initialization
     startTime = "2019-04-20T00:00:00Z"
-    endTime = "2019-04-20T01:00:00Z"
-    timeInterval = "5m"
+    endTime = "2019-04-20T23:59:59Z"
+    timeInterval = "30m"
     valueType = "MEAN"
-    hostIp = "10.101.1.1"
+    hostIp = "10.101.9.6"
     client = InfluxDBClient(
         host='localhost', 
         port=8086, 
@@ -26,18 +26,18 @@ def main():
     print("Writing result into file...")
     with open("./influxdb/table.json", "w") as outfile:
         # Query BMC metrics
-        for m in measure_bmc_list:
-            print("Get metric: {} from BMC...\n".format(m))
-            bmc_metric = query_bmc(
-                client, hostIp, m, valueType, startTime, endTime, timeInterval
-            )
-            json.dump(bmc_metric[0], outfile)
+        # for m in measure_bmc_list:
+        #    print("Get metric: {} from BMC...\n".format(m))
+        #    bmc_metric = query_bmc(
+        #        client, hostIp, m, valueType, startTime, endTime, timeInterval
+        #    )
+        #    json.dump(bmc_metric[0], outfile)
         # Query UGE metrics
         print("Get metric: Job_Info from UGE...\n")
         uge_metric = query_uge(
             client, hostIp, startTime, endTime, timeInterval
         )
-        json.dump(uge_metric[0], outfile)
+        json.dump(uge_metric, outfile)
     print("Done!")
             
 if __name__ == "__main__":
