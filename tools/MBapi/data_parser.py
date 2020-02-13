ffd762556ba645356ec1b2f48af00205e732df31
artlands <lijie0416@gmail.com>
2020-02-12 22:10:25 -0600

    debug parser

--- tools/MBapi/data_parser.py
+++ tools/MBapi/data_parser.py
@@ -2,16 +2,15 @@ def node_data_parser(node_list: list, node_data: dict) -> dict:
     json_data = {}
     fields = ["CPU1_temp", "CPU2_temp", "CPUCores", "cpuusage", "fan1_speed", "fan2_speed", "fan3_speed", "fan4_speed", "inlet_temp", "jobID", "memoryusage", "powerusage_watts", "jobID"]
     for node in node_list:
-        # print(node)
         json_data[node] = {}
         for field in fields:
             json_data[node][field] = []
             if field == "jobID":
-                for item in node_data[field]:
+                for item in node_data[node][field]:
                     job_arr = de_duplicate(item["distinct"])
                     json_data[node][field].append(job_arr)
             else:
-                for item in node_data[field]:
+                for item in node_data[node][field]:
                     json_data[node][field].append(item['max'])
         
     print(json_data[node])
