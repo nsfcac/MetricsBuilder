8b4b38672879ed4eb73959665593054c65107ddb
artlands <lijie0416@gmail.com>
2020-02-13 13:16:19 -0600

    debug

--- tools/MBapi/write_csv.py
+++ tools/MBapi/write_csv.py
@@ -45,58 +45,58 @@ def build_csv(json_data: dict, hostfile: str, jobfile: str) -> None:
 
     print("Time Stamp length: ", len(time_List))
 
-    host_list = list(hostDetail.keys())
-    feature_list = list(hostDetail[host_list[0]].keys())
+    # host_list = list(hostDetail.keys())
+    # feature_list = list(hostDetail[host_list[0]].keys())
 
-    for host in host_list:
-        for feature in feature_list:
-            print(host, "-", feature, " : ", len(hostDetail[host][feature]))
-
-    # # Write host details into a CSV file
-    # with open(hostfile, "w") as host_csv_file:
-    #     header_list = ["TimeStamp"]
-    #     host_list = list(hostDetail.keys())
-    #     feature_list = list(hostDetail[host_list[0]].keys())
-    #     csvwriter = csv.writer(host_csv_file)
-
-    #     # Write header
-    #     for host in host_list:
-    #         for feature in feature_list:
-    #             header = host + "-" + feature
-    #             header_list.append(header)   
-        
-    #     print("header length: ", len(header_list))
-    #     csvwriter.writerow(header_list)
-
-    #     # Write value
-    #     for i, timestamp in enumerate(time_List):
-    #         each_row = [timestamp]
-    #         for host in host_list:
-    #             # print(host)
-    #             for feature in feature_list:
-    #                 print(hostDetail[host][feature][i])
-    #                 each_row.append(hostDetail[host][feature][i])
-    #         csvwriter.writerow(each_row)
-    
-    # # Write job details into a CSV file
-    # with open(jobfile, "w") as job_csv_file:
-    #     header_list = ["JobID"]
-    #     job_list = list(jobDetail.keys())
-    #     feature_list = list(jobDetail[job_list[0]].keys())
-
-    #     csvwriter = csv.writer(job_csv_file)
-    #     # Write header
+    # for host in host_list:
     #     for feature in feature_list:
-    #         header_list.append(feature)
-        
-    #     csvwriter.writerow(header_list)
+    #         print(host, "-", feature, " : ", len(hostDetail[host][feature]))
 
-    #     # Write value
-    #     for job in job_list:
-    #         each_row = [job]
-    #         for feature in feature_list:
-    #             each_row.append(jobDetail[job][feature])
-    #         csvwriter.writerow(each_row)
+    # Write host details into a CSV file
+    with open(hostfile, "w") as host_csv_file:
+        header_list = ["TimeStamp"]
+        host_list = list(hostDetail.keys())
+        feature_list = list(hostDetail[host_list[0]].keys())
+        csvwriter = csv.writer(host_csv_file)
+
+        # Write header
+        for host in host_list:
+            for feature in feature_list:
+                header = host + "-" + feature
+                header_list.append(header)   
+        
+        print("header length: ", len(header_list))
+        csvwriter.writerow(header_list)
+
+        # Write value
+        for i, timestamp in enumerate(time_List):
+            each_row = [timestamp]
+            for host in host_list:
+                # print(host)
+                for feature in feature_list:
+                    print(hostDetail[host][feature][i])
+                    each_row.append(hostDetail[host][feature][i])
+            csvwriter.writerow(each_row)
+    
+    # Write job details into a CSV file
+    with open(jobfile, "w") as job_csv_file:
+        header_list = ["JobID"]
+        job_list = list(jobDetail.keys())
+        feature_list = list(jobDetail[job_list[0]].keys())
+
+        csvwriter = csv.writer(job_csv_file)
+        # Write header
+        for feature in feature_list:
+            header_list.append(feature)
+        
+        csvwriter.writerow(header_list)
+
+        # Write value
+        for job in job_list:
+            each_row = [job]
+            for feature in feature_list:
+                each_row.append(jobDetail[job][feature])
+            csvwriter.writerow(each_row)
 
 
 if __name__ == "__main__":
