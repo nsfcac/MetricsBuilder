e5b4aeeaefa8398b6f218cfe779ee3b689b7f924
artlands <lijie0416@gmail.com>
2020-02-19 10:59:16 -0600

    test modification

--- tools/MBapi/process_data.py
+++ tools/MBapi/process_data.py
@@ -69,10 +69,10 @@ def process_node_data(node_list: list, node_data: dict, time_list: list, value: 
             # process jobID
             for item in node_data[node]["jobID"]:
                 if item["time"] == time:
-                    if json_data[node]["job_id"][i]:
+                    try:
                         tmp = set(json_data[node]["job_id"][i] + item["distinct"])
                         json_data[node]["job_id"][i] = list(tmp)
-                    else:
+                    except:
                         json_data[node]["job_id"].append([])
                         json_data[node]["job_id"][i] = list(set(item["distinct"]))
                 else:
