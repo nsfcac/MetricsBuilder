2caadebd3c06d9c8e68c859369a13bab640417cb
artlands <lijie0416@gmail.com>
2020-03-10 14:40:00 -0500

    first data point

--- tools/MBConvertor/query_db.py
+++ tools/MBConvertor/query_db.py
@@ -7,7 +7,10 @@ def get_fst_time(client: object) -> object:
     # sec_mea = ["cluster_unified_metrics", "Current_Jobs_ID"]
 
     try:            
-        sql = """ SELECT first("CPU1 Temp") FROM "CPU_Temperature" WHERE host="10.101.1.1" """
+        field = "CPU1 Temp"
+        measurement = "CPU_Temperature"
+        host = "10.101.1.1"
+        sql = "SELECT first(" + field + ") FROM " + measurement + "WHERE host = '" + host + "'"
         data_point = client.get(sql)
         return data_point
     except Exception as err:
