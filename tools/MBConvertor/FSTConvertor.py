a6a8c174d77f7c469f0a9276088484cd73f81ba1
artlands <lijie0416@gmail.com>
2020-03-10 21:09:38 -0500

    test FSTConvertor

--- tools/MBConvertor/FSTConvertor.py
+++ tools/MBConvertor/FSTConvertor.py
@@ -6,7 +6,7 @@ import json
 
 from DBcm import QueryInfluxdb
 from parse_config import parse_host
-from query_db import get_phase_time, query_data_point
+from query_db import get_phase_time, query_data, query_data_point
 
 read_config = {
     'host': 'localhost',
@@ -29,8 +29,17 @@ def main():
     phase_time = get_phase_time(read_client)
     print(phase_time)
 
-    data_point = query_data_point(read_client)
-    print(json.dumps(data_point, indent=4))
+    start = phase_time[0]
+    one_day = 24 * 60 * 60 * 1000
+    end = start + one_day
+
+    measurement = "CPU_Temperature"
+    node_list = ["10.101.1.1"]
+
+    data = query_data(node_list, measurement, read_client, start, end)
+    print(data[0])
+    # data_point = query_data_point(read_client)
+    # print(json.dumps(data_point, indent=4))
     # Get host list
     # hostlist = parse_host()
     # Get first timestamp
--- tools/MBConvertor/process_data.py
+++ tools/MBConvertor/process_data.py
@@ -1,80 +1,96 @@
 def process_data(json_data: dict, measurement: str) -> list:
     # fst_mea = ["CPU_Temperature", "Inlet_Temperature", "CPU_Usage", 
     #         "Memory_Usage", "Fan_Speed", "Node_Power_Usage"]
+    result = []
     try:
         for data in json_data:
             if measurement == "CPU_Temperature":
-                data_point = {
+                data_point_1 = {
                     "measurement" : "Thermal",
-                    "time": data,
+                    "time": data["time"],
                     "tags": {
-                        "Sensor": data,
-                        "NodeId": data
+                        "Sensor": "CPU1Temp",
+                        "NodeId": data["host"]
                     }, 
                     "fields": {
-                        "Reading": data
+                        "Reading": data["CPU1 Temp"]
                     }
                 }
-            if measurement == "Inlet_Temperature":
-                data_point = {
+                data_point_2 = {
                     "measurement" : "Thermal",
-                    "time": data,
+                    "time": data["time"],
                     "tags": {
-                        "Sensor": "InletTemp",
-                        "NodeId": data
+                        "Sensor": "CPU2Temp",
+                        "NodeId": data["host"]
                     }, 
                     "fields": {
-                        "Reading": data
+                        "Reading": data["CPU2 Temp"]
                     }
                 }
-            if measurement == "CPU_Usage":
-                data_point = {
-                    "measurement" : "UGE",
-                    "time": data,
-                    "tags": {
-                        "Sensor": "CPUUsage",
-                        "NodeId": data
-                    }, 
-                    "fields": {
-                        "Reading": data
-                    }
-                }
-            if measurement == "Memory_Usage":
-                data_point = {
-                    "measurement" : "UGE",
-                    "time": data,
-                    "tags": {
-                        "Sensor": "MemUsage",
-                        "NodeId": data
-                    }, 
-                    "fields": {
-                        "Reading": data
-                    }
-                }
-            if measurement == "Fan_Speed":
-                data_point = {
-                    "measurement" : "Thermal",
-                    "time": data,
-                    "tags": {
-                        "Sensor": "FAN1",
-                        "NodeId": data
-                    }, 
-                    "fields": {
-                        "Reading": data
-                    }
-                }
-            if measurement == "Node_Power_Usage":
-                data_point = {
-                    "measurement" : "Power",
-                    "time": data,
-                    "tags": {
-                        "Sensor": "NodePower",
-                        "NodeId": data
-                    }, 
-                    "fields": {
-                        "Reading": data
-                    }
-                }
+                result.append(data_point_1)
+                result.append(data_point_2)
+            # if measurement == "Inlet_Temperature":
+            #     data_point = {
+            #         "measurement" : "Thermal",
+            #         "time": data,
+            #         "tags": {
+            #             "Sensor": "InletTemp",
+            #             "NodeId": data
+            #         }, 
+            #         "fields": {
+            #             "Reading": data
+            #         }
+            #     }
+            # if measurement == "CPU_Usage":
+            #     data_point = {
+            #         "measurement" : "UGE",
+            #         "time": data,
+            #         "tags": {
+            #             "Sensor": "CPUUsage",
+            #             "NodeId": data
+            #         }, 
+            #         "fields": {
+            #             "Reading": data
+            #         }
+            #     }
+            # if measurement == "Memory_Usage":
+            #     data_point = {
+            #         "measurement" : "UGE",
+            #         "time": data,
+            #         "tags": {
+            #             "Sensor": "MemUsage",
+            #             "NodeId": data
+            #         }, 
+            #         "fields": {
+            #             "Reading": data
+            #         }
+            #     }
+            # if measurement == "Fan_Speed":
+            #     data_point = {
+            #         "measurement" : "Thermal",
+            #         "time": data,
+            #         "tags": {
+            #             "Sensor": "FAN1",
+            #             "NodeId": data
+            #         }, 
+            #         "fields": {
+            #             "Reading": data
+            #         }
+            #     }
+            # if measurement == "Node_Power_Usage":
+            #     data_point = {
+            #         "measurement" : "Power",
+            #         "time": data,
+            #         "tags": {
+            #             "Sensor": "NodePower",
+            #             "NodeId": data
+            #         }, 
+            #         "fields": {
+            #             "Reading": data
+            #         }
+            #     }
     except Exception as err:
         print(err)
+    
+    return result
 
