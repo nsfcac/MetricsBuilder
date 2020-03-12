353f58301f0abb2a79f9d960cc0730da3340bc9d
artlands <lijie0416@gmail.com>
2020-03-12 14:34:55 -0500

    test convertor

--- tools/MBConvertor/FSTConvertor.py
+++ tools/MBConvertor/FSTConvertor.py
@@ -102,8 +102,9 @@ def main():
     data_points = []
     for mea in job_measurements:
         json_data = query_data_job(mea, read_client)
-        if json_data:
-            data_points.extend(process_data_job(json_data[0], mea))
+        print(json_data)
+        # if json_data:
+        #     data_points.extend(process_data_job(json_data[0], mea))
 
     print(json.dumps(data_points, indent=4))
 
