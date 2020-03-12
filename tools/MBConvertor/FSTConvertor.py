487c8f87a9c831cd62a485d96cb89a43facc0886
artlands <lijie0416@gmail.com>
2020-03-12 15:07:50 -0500

    test convertor

--- tools/MBConvertor/FSTConvertor.py
+++ tools/MBConvertor/FSTConvertor.py
@@ -94,23 +94,38 @@ def main():
 
     # Convert system metrics
     # data_points = []
-    for mea in sys_measurements:
-        print(f"Converting {mea}...")
-        json_data = query_data(mea, read_client, st, et)
-        if json_data:
-            print(f"Original data point:")
-            print(json.dumps(json_data[0], indent=4))
 
-            converted_data_point = process_data(json_data[0], mea)
-            print(f"Converted data point:")
-            print(json.dumps(converted_data_point, indent=4))
+    process_dict = [
+        "CPU_Temperature",
+        "CPU_Usage",
+        "Fan_Speed",
+        "Inlet_Temperature",
+        "Job_Info",
+        "Memory_Usage",
+        "Node_Power_Usage",
+        "cluster_unified_metrics",
+        "node_job_info",
+        "system_metrics"
+    ]
+
+    for mea in sys_measurements:
+        if mea in process_dict:
+            print(f"Converting {mea}...")
+            json_data = query_data(mea, read_client, st, et)
+            if json_data:
+                print(f"Original data point:")
+                print(json.dumps(json_data[0], indent=4))
+
+                converted_data_point = process_data(json_data[0], mea)
+                print(f"Converted data point:")
+                print(json.dumps(converted_data_point, indent=4))
 
             # data_points.append(converted_data_point)
     # print(json.dumps(data_points, indent=4))
 
     # Convert job metrics
     job_measurements = ["i764687", "j-775882", "qu_1082110A434"]
-    data_points = []
+    # data_points = []
     for mea in job_measurements:
         print(f"Converting {mea}...")
         json_data = query_data_job(mea, read_client)
