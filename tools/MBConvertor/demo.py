770e0e471fbe877c656f95d6f1f67a439d2eef23
artlands <lijie0416@gmail.com>
2020-03-13 11:21:51 -0500

    test unified

--- tools/MBConvertor/demo.py
+++ tools/MBConvertor/demo.py
@@ -39,6 +39,7 @@ def demo(read_client: object, sys_measurements: list, job_measurements: list) ->
     for mea in sys_measurements:
         if mea in process_dict:
             json_data = query_data(mea, read_client, st, et)
+            print(json_data[0])
             if json_data:
                 print(f"Converting {mea}...")
                 print("---- Original data point ----")
@@ -51,18 +52,18 @@ def demo(read_client: object, sys_measurements: list, job_measurements: list) ->
             # data_points.append(converted_data_point)
     # print(json.dumps(data_points, indent=4))
 
-    # Convert job metrics
-    job_measurements = ["i764687", "j-775882", "qu_1082110A434"]
-    # data_points = []
-    for mea in job_measurements:
-        print(f"Converting {mea}...")
-        json_data = query_data_job(mea, read_client)
-        if json_data:
-            print("---- Original data point ----")
-            print(json.dumps(json_data, indent=4))
+    # # Convert job metrics
+    # job_measurements = ["i764687", "j-775882", "qu_1082110A434"]
+    # # data_points = []
+    # for mea in job_measurements:
+    #     print(f"Converting {mea}...")
+    #     json_data = query_data_job(mea, read_client)
+    #     if json_data:
+    #         print("---- Original data point ----")
+    #         print(json.dumps(json_data, indent=4))
 
-            converted_data_point = process_data_job(json_data, mea)
-            print("---- Converted data point ----")
-            print(json.dumps(converted_data_point, indent=4))
-            print("-------------------------------------------------------")
+    #         converted_data_point = process_data_job(json_data, mea)
+    #         print("---- Converted data point ----")
+    #         print(json.dumps(converted_data_point, indent=4))
+    #         print("-------------------------------------------------------")
     return
