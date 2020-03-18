6b48d7bf543c86b6ca7bf0975172c3e62895f97f
artlands <lijie0416@gmail.com>
2020-03-17 20:56:08 -0500

    test api

--- tools/MBopenapi/mbapi_server/openapi_server/controllers/demo.py
+++ tools/MBopenapi/mbapi_server/openapi_server/controllers/demo.py
@@ -27,31 +27,13 @@ def get_metrics(start, end, interval, value):  # noqa: E501
     else:
 
         query_start = time.time()
-        # Get time stamp
-        # time_list = gen_timestamp(start, end, interval)
 
-        # Query Nodes and Jobs info
         all_data = query_data(node_list, influx, start_str, end_str, interval, value)
 
         query_elapsed = float("{0:.2f}".format(time.time() - query_start))
 
         print(query_elapsed)
         print(json.dumps(all_data, indent=4))
-
-        # process_start = time.time()
-        # # Process Nodes and Jobs info
-        # unified_metrics.JobsInfo = process_job_data(all_data["job_data"])
-        # unified_metrics.NodesInfo = process_node_data(node_list, all_data["node_data"], time_list, value)
-
-        # process_elapsed = float("{0:.2f}".format(time.time() - process_start)) 
-        # total_elapsed = float("{0:.2f}".format(query_elapsed + process_elapsed))
-        # # In seconds
-        # time_range = int(end.timestamp()) - int(start.timestamp())
-
-        # with open("requests.log", "a+") as requests_log:
-        #     print(f"{time_range}:{interval}:{value}:{query_elapsed}:{process_elapsed}:{total_elapsed}", file = requests_log)
-
-    # return unified_metrics
 
 
 if __name__ == "__main__":
