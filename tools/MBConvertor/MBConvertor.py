5aa9239b44ffb1851de428727dfbd39f53e714b2
artlands <lijie0416@gmail.com>
2020-03-18 14:42:27 -0500

    test

--- tools/MBConvertor/MBConvertor.py
+++ tools/MBConvertor/MBConvertor.py
@@ -25,7 +25,7 @@ read_config = {
 write_config = {
     'host': 'localhost',
     'port': '8086',
-    'database': 'test_schema' #  updated_schema
+    'database': 'updated_schema'
 }
 
 def main():
@@ -40,9 +40,8 @@ def main():
     # phase_time = get_phase_time(read_client)
 
     first =  1552539600
-    last = 1552626000
-    # last = 1583301600
-    step = 1 * 60 * 60
+    last = 1583301600
+    step = 0.5 * 60 * 60
     
     # Get all system measurements
     # print("Analysis measurements...")
@@ -50,11 +49,11 @@ def main():
     sys_measurements = measurements["sys_measurements"]
     job_measurements = measurements["job_measurements"]
 
-    # # Converting job metrics in parallel
-    # convert_data_job_args = zip(repeat(read_client), repeat(write_client), 
-    #                         job_measurements, repeat(error_count))
-    # with multiprocessing.Pool(processes=cpu_count) as pool:
-    #     pool.starmap(convert_data_job, convert_data_job_args)
+    # Converting job metrics in parallel
+    convert_data_job_args = zip(repeat(read_client), repeat(write_client), 
+                            job_measurements, repeat(error_count))
+    with multiprocessing.Pool(processes=cpu_count) as pool:
+        pool.starmap(convert_data_job, convert_data_job_args)
 
     # Converting system metrics in parallel
     for start in range(first, last, step):
