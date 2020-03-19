9e39501947e3cfef916a2ac710a076fed0a05918
artlands <lijie0416@gmail.com>
2020-03-18 23:41:21 -0500

    test

--- tools/MBopenapi/mbapi_server/openapi_server/controllers/query_db.py
+++ tools/MBopenapi/mbapi_server/openapi_server/controllers/query_db.py
@@ -15,22 +15,17 @@ def query_data(node_list: list, influx: object, start: str, end: str, interval: 
 
     # Get nodes metrics
     for node in node_list:
+        print(type(node))
         node_data[node] = {}
         for label in thermal_labels:
             reading = query_reading(influx, node, "Thermal", label, start, end, interval, value)
-            json_data[node].update({
-                label: reading
-            })
+            json_data[node][label] = reading
         for label in uge_labels:
             reading = query_reading(influx, node, "UGE", label, start, end, interval, value) 
-            json_data[node].update({
-                label: reading
-            })
+            json_data[node][label] = reading
         for label in power_labels:
             reading = query_reading(influx, node, "Power", label, start, end, interval, value)
-            json_data[node].update({
-                label: reading
-            })
+            json_data[node][label] = reading
         # print(json.dumps(node_data[node], indent=4))
     #     job_list = query_job_list(influx, node, start, end)
     #     node_data[node]["JobList"] = job_list
@@ -47,7 +42,7 @@ def query_data(node_list: list, influx: object, start: str, end: str, interval: 
     #     "node_data": node_data,
     #     "job_data": job_data
     # })
-    print(json.dumps(node_data, indent=4))
+    # print(json.dumps(node_data, indent=4))
     # except Exception as err:
     #     print(err)
     return json_data
