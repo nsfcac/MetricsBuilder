02005a6fe55d01b02b1abab295bb95784aee868e
artlands <lijie0416@gmail.com>
2020-03-18 23:18:14 -0500

    demo

--- tools/MBopenapi/mbapi_server/openapi_server/controllers/demo.py
+++ tools/MBopenapi/mbapi_server/openapi_server/controllers/demo.py
@@ -2,14 +2,15 @@ import connexion
 import six
 import time
 import json
+import datetime
 
 from parse_config import parse_conf
 from gen_timestamp import gen_timestamp, gen_epoch_timestamp
 from DBcm import QueryInfluxdb
 from query_db import query_data
 
-start = 1581469200
-end = 1581472800
+start = 1552539600 + 24 * 60 * 60 * 2
+end = 1552539600 + 24 * 60 * 60 * 3
 interval = "5m"
 value = "max"
 
@@ -17,17 +18,17 @@ value = "max"
 # Initialization 
 config = parse_conf()
 node_list = ['10.101.1.1']
-print(config["influxdb"])
+# print(config["influxdb"])
 influx = QueryInfluxdb(config["influxdb"])
 
 # Time string used in query_data
-start_epoch = start
-end_epoch = end
+st = datetime.datetime.utcfromtimestamp(start).strftime('%Y-%m-%dT%H:%M:%SZ')
+et = datetime.datetime.utcfromtimestamp(end).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-print(f"Start time: {start_epoch}; End time: {end_epoch}")
+print(f"Start time: {st}; End time: {et}")
 
 # Check Sanity
 
-all_data = query_data(node_list, influx, start_epoch, end_epoch, interval, value)
+all_data = query_data(node_list, influx, st, et, interval, value)
 
 print(json.dumps(all_data, indent=4))
