2401f931ee0a53d41b3f9cfdddd9b20e9bec2bfc
artlands <lijie0416@gmail.com>
2020-03-18 23:22:16 -0500

    test

--- tools/MBopenapi/mbapi_server/openapi_server/controllers/query_db.py
+++ tools/MBopenapi/mbapi_server/openapi_server/controllers/query_db.py
@@ -53,6 +53,7 @@ def query_reading(influx: object, node: str, measurement: str, label: str,
                     + " WHERE Label='" + label + "' AND NodeId='" + node \
                     + "' AND time >= '" + start + " AND time < '" + end \
                     + " GROUP BY time(" + interval + ") fill(null)"
+        print(query_sql)
         reading = influx.get(query_sql)
     except Exception as err:
         print(err)
