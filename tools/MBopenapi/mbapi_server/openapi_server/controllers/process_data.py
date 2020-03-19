4f5cc601212fb83ed35ccff0d5a3cf3ca15aed71
artlands <lijie0416@gmail.com>
2020-03-19 16:49:49 -0500

    test

--- tools/MBopenapi/mbapi_server/openapi_server/controllers/process_data.py
+++ tools/MBopenapi/mbapi_server/openapi_server/controllers/process_data.py
@@ -57,8 +57,9 @@ def process_node_data(node_list: list, node_data: dict, value: str) -> dict:
                 else:
                     fan_speed[index].append(None)
             
-            time_list = [item["time"] for item in node_data[node]["JobList"]]
+            time_list = list(set([item["time"] for item in node_data[node]["JobList"]]))
             print(time_list)
+            print(len(time_list))
 
             JobListStr = [item["distinct"][1:-1].split(", ") for item in node_data[node]["JobList"]]
             JobList = []
