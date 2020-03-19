a025585053a47e101a8264248239fb7ea0e54b88
artlands <lijie0416@gmail.com>
2020-03-19 13:17:02 -0500

    test

--- tools/MBopenapi/mbapi_server/openapi_server/controllers/test.py
+++ tools/MBopenapi/mbapi_server/openapi_server/controllers/test.py
@@ -11,9 +11,9 @@ from query_db import query_data
 from process_data import  process_node_data
 
 
-start = 1552539600
-end = 1552539600 + 24 * 60 * 60
-interval = "2h"
+start = 1564102552
+end = 1564102552 + 1 * 60 * 60
+interval = "5m"
 value = "max"
 
 
