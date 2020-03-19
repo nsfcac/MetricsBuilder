209ac6c42cefbc9b58cb83cb9f9b50e704acdd38
artlands <lijie0416@gmail.com>
2020-03-19 00:40:50 -0500

    test

--- tools/MBopenapi/mbapi_server/openapi_server/controllers/process_data.py
+++ tools/MBopenapi/mbapi_server/openapi_server/controllers/process_data.py
@@ -18,9 +18,18 @@ def process_all_data(node_list: list, all_data: dict, value: str) -> dict:
         cpu_inl_temp = []
         for index, item in enumerate(CPU1Temp):
             cpu_inl_temp.append([])
-            cpu_inl_temp[index].append(item)
-            cpu_inl_temp[index].extend(CPU2Temp[index])
-            cpu_inl_temp[index].extend(InletTemp[index])
+            if item:
+                cpu_inl_temp[index].append(item)
+            else:
+                cpu_inl_temp[index].append(None)
+            if CPU2Temp[index]:
+                cpu_inl_temp[index].extend(CPU2Temp[index])
+            else:
+                cpu_inl_temp[index].extend(None)
+            if InletTemp[index]:
+                cpu_inl_temp[index].extend(InletTemp[index])
+            else:
+                cpu_inl_temp[index].extend(None)
 
         FAN_1 = [item[value] for item in all_data["node_data"][node]["FAN_1"]]
         FAN_2 = [item[value] for item in all_data["node_data"][node]["FAN_2"]]
@@ -30,11 +39,23 @@ def process_all_data(node_list: list, all_data: dict, value: str) -> dict:
         fan_speed = []
         for index, item in enumerate(FAN_1):
             fan_speed.append([])
-            fan_speed[index].append(item)
-            fan_speed[index].append(FAN_2[index])
-            fan_speed[index].append(FAN_3[index])
-            fan_speed[index].append(FAN_4[index])
-    
+            if item:
+                fan_speed[index].append(item)
+            else:
+                fan_speed[index].append(None)
+            if FAN_2[index]:
+                fan_speed[index].append(FAN_2[index])
+            else:
+                fan_speed[index].append(None)
+            if FAN_3[index]:
+                fan_speed[index].append(FAN_3[index])
+            else:
+                fan_speed[index].append(None)
+            if FAN_4[index]:
+                fan_speed[index].append(FAN_4[index])
+            else:
+                fan_speed[index].append(None)
+                
         json_data[node] = {
             "memory_usage": memory_usage,
             "cpu_usage": cpu_usage,
