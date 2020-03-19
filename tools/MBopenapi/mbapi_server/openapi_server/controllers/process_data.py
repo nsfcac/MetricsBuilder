e0ae1c207542496b5dcb706d130ee69e94af8648
artlands <lijie0416@gmail.com>
2020-03-19 00:34:22 -0500

    test

--- tools/MBopenapi/mbapi_server/openapi_server/controllers/process_data.py
+++ tools/MBopenapi/mbapi_server/openapi_server/controllers/process_data.py
@@ -1,19 +1,19 @@
 import time
 
-def process_node_data(node_list: list, node_data: dict, value: str) -> dict:
+def process_all_data(node_list: list, all_data: dict, value: str) -> dict:
     """
     Process node data retrieved from influxdb
     """
     json_data = {}
     # try:
     for node in node_list:
-        memory_usage = [item[value] for item in node_data[node]["node_data"]["MemUsage"]]
-        cpu_usage = [item[value] for item in node_data[node]["node_data"]["CPUUsage"]]
-        power_usage = [item[value] for item in node_data[node]["node_data"]["NodePower"]]
+        memory_usage = [item[value] for item in all_data["node_data"][node]["MemUsage"]]
+        cpu_usage = [item[value] for item in all_data["node_data"][node]["CPUUsage"]]
+        power_usage = [item[value] for item in all_data["node_data"][node]["NodePower"]]
         
-        CPU1Temp = [item[value] for item in node_data[node]["node_data"]["CPU1Temp"]]
-        CPU2Temp = [item[value] for item in node_data[node]["node_data"]["CPU2Temp"]]
-        InletTemp = [item[value] for item in node_data[node]["node_data"]["InletTemp"]]
+        CPU1Temp = [item[value] for item in all_data["node_data"][node]["CPU1Temp"]]
+        CPU2Temp = [item[value] for item in all_data["node_data"][node]["CPU2Temp"]]
+        InletTemp = [item[value] for item in all_data["node_data"][node]["InletTemp"]]
 
         cpu_inl_temp = []
         for index, item in enumerate(CPU1Temp):
@@ -22,10 +22,10 @@ def process_node_data(node_list: list, node_data: dict, value: str) -> dict:
             cpu_inl_temp[index].extend(CPU2Temp[index])
             cpu_inl_temp[index].extend(InletTemp[index])
 
-        FAN_1 = [item[value] for item in node_data[node]["node_data"]["FAN_1"]]
-        FAN_2 = [item[value] for item in node_data[node]["node_data"]["FAN_2"]]
-        FAN_3 = [item[value] for item in node_data[node]["node_data"]["FAN_3"]]
-        FAN_4 = [item[value] for item in node_data[node]["node_data"]["FAN_4"]]
+        FAN_1 = [item[value] for item in all_data["node_data"][node]["FAN_1"]]
+        FAN_2 = [item[value] for item in all_data["node_data"][node]["FAN_2"]]
+        FAN_3 = [item[value] for item in all_data["node_data"][node]["FAN_3"]]
+        FAN_4 = [item[value] for item in all_data["node_data"][node]["FAN_4"]]
 
         fan_speed = []
         for index, item in enumerate(FAN_1):
@@ -41,7 +41,7 @@ def process_node_data(node_list: list, node_data: dict, value: str) -> dict:
             "power_usage": power_usage,
             "fan_speed": fan_speed,
             "cpu_inl_temp": cpu_inl_temp,
-            "job_id": node_data[node]["job_data"]
+            "job_id": all_data["job_data"]
         }
     # except Exception as err:
     #     print(err)
