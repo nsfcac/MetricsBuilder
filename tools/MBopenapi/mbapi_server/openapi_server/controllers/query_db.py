b8d03d7f53dabd58fa35118f44b96a58fdfdf4fd
artlands <lijie0416@gmail.com>
2020-03-19 21:14:49 -0500

    test

--- tools/MBopenapi/mbapi_server/openapi_server/controllers/query_db.py
+++ tools/MBopenapi/mbapi_server/openapi_server/controllers/query_db.py
@@ -81,14 +81,14 @@ def query_node_data(node:str, influx: object, start: str, end: str,
 def query_reading(influx: object, node: str, measurement: str, label: str, 
                   start: str, end: str, interval: str, value: str) -> list:
     reading = []
-    try:
-        query_sql = "SELECT " + value + "(Reading) FROM " + measurement \
-                    + " WHERE Label='" + label + "' AND NodeId='" + node \
-                    + "' AND time >= '" + start + "' AND time < '" + end \
-                    + "' GROUP BY time(" + interval + ") fill(null)"
-        reading = influx.get(query_sql)
-    except Exception as err:
-        print(err)
+    # try:
+    query_sql = "SELECT " + value + "(Reading) FROM " + measurement \
+                + " WHERE Label='" + label + "' AND NodeId='" + node \
+                + "' AND time >= '" + start + "' AND time < '" + end \
+                + "' GROUP BY time(" + interval + ") fill(null)"
+    reading = influx.get(query_sql)
+    # except Exception as err:
+    #     print(err)
     return reading
 
 def query_job_list(influx: object, node: str, 
