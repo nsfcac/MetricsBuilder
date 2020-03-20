ab023cdf0ca0070b5d84ed95790fe4d29ad36e23
artlands <lijie0416@gmail.com>
2020-03-19 21:18:05 -0500

    test

--- tools/MBopenapi/mbapi_server/openapi_server/controllers/query_db.py
+++ tools/MBopenapi/mbapi_server/openapi_server/controllers/query_db.py
@@ -61,12 +61,15 @@ def query_node_data(node:str, influx: object, start: str, end: str,
         print(node)
         # Get node metrics
         for label in thermal_labels:
+            print(label)
             reading = query_reading(influx, node, "Thermal", label, start, end, interval, value)
             node_data[label] = reading
         for label in uge_labels:
+            print(label)
             reading = query_reading(influx, node, "UGE", label, start, end, interval, value) 
             node_data[label] = reading
         for label in power_labels:
+            print(label)
             reading = query_reading(influx, node, "Power", label, start, end, interval, value)
             node_data[label] = reading
         job_list = query_job_list(influx, node, start, end, interval)
@@ -87,7 +90,7 @@ def query_reading(influx: object, node: str, measurement: str, label: str,
                     + "' AND time >= '" + start + "' AND time < '" + end \
                     + "' GROUP BY time(" + interval + ") fill(null)"
         reading = influx.get(query_sql)
-        print(query_sql)
+        # print(query_sql)
     except Exception as err:
         print(err)
     return reading
