a7a201531f2eb13d9cc38a789e0d94657e23d9f0
artlands <lijie0416@gmail.com>
2020-03-19 22:25:47 -0500

    test

--- tools/MBopenapi/mbapi_server/openapi_server/controllers/test.py
+++ tools/MBopenapi/mbapi_server/openapi_server/controllers/test.py
@@ -12,17 +12,17 @@ from query_db import query_data, query_node_data
 from process_data import process_node_data
 
 
-hours = 24
-start = 1564660800
-end = 1564660800 + hours * 60 * 60
-interval = "30m"
+hours = 24 * 3
+start = 1564228800
+end = 1564228800 + hours * 60 * 60
+interval = "5m"
 value = "max"
 
 
 # Initialization 
 config = parse_conf()
-# node_list = parse_host()
-node_list = ["10.101.1.1", "10.101.1.2"]
+node_list = parse_host()
+# node_list = ["10.101.1.1", "10.101.1.2"]
 # print(config["influxdb"])
 influx = QueryInfluxdb(config["influxdb"])
 
@@ -32,28 +32,27 @@ et = datetime.datetime.utcfromtimestamp(end).strftime('%Y-%m-%dT%H:%M:%SZ')
 
 # print(f"Start time: {st}; End time: {et}")
 
-# Check Sanity
-out_queue = mp.Queue()
+# out_queue = mp.Queue()
 
 query_start = time.time()
 
-print("Start Processes...")
-workers = [mp.Process(target=query_node_data, args=(node, influx, st , et, interval, value, out_queue)) for node in node_list]
+# workers = [mp.Process(target=query_node_data, args=(node, influx, st , et, interval, value, out_queue)) for node in node_list]
 
-for worker in workers:
-    worker.start()
-for worker in workers:
-    worker.join()
+# for worker in workers:
+#     worker.start()
+# for worker in workers:
+#     worker.join()
 
-all_data = {}
-for index, host in enumerate(node_list):
-    all_data[host] = out_queue.get()
+# all_data = {}
+# for index, host in enumerate(node_list):
+#     all_data[host] = out_queue.get()
 
-print(json.dumps(all_data, indent=4))
-# all_data = query_data(node_list, influx, st, et, interval, value)
+# print(json.dumps(all_data, indent=4))
+
+all_data = query_data(node_list, influx, st, et, interval, value)
 
 query_elapsed = float("{0:.2f}".format(time.time() - query_start))
-print(f"(Parallel) Time for Quering {hours} of data : {query_elapsed}")
+print(f"Time for Quering {hours} of data : {query_elapsed}")
 # print(json.dumps(all_data["job_data"], indent=4))
 
 # process_start = time.time()
