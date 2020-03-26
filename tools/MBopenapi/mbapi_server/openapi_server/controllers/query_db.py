ea414ce523b4f1b302ee9e0af910ecc0e45a07c6
artlands <lijie0416@gmail.com>
2020-03-25 22:46:20 -0500

    test

--- tools/MBopenapi/mbapi_server/openapi_server/controllers/query_db.py
+++ tools/MBopenapi/mbapi_server/openapi_server/controllers/query_db.py
@@ -8,7 +8,8 @@ def query_process_data(node:str, influx: object, start: str, end: str,
     json_data = {}
     try:
         node_data = query_node_data(node, influx, start, end, interval, value)
-        json_data = process_node_data(node, node_data, value)
+        if node_data:
+            json_data = process_node_data(node, node_data, value)
     except Exception as err:
         print(err)
     return json_data
