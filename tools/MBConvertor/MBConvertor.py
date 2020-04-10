2df6d2a769469855a5a34d525c21c7232a8ceb7b
artlands <lijie0416@gmail.com>
2020-04-10 16:27:34 -0500

    update MBConvertor

--- tools/MBConvertor/MBConvertor.py
+++ tools/MBConvertor/MBConvertor.py
@@ -52,7 +52,7 @@ def main():
     # print("Analysis measurements...")
     measurements = parse_measurement(read_client)
     sys_measurements = measurements["sys_measurements"]
-    job_measurements = measurements["job_measurements"]
+    # job_measurements = measurements["job_measurements"]
 
     # # Converting job metrics in parallel
     # convert_data_job_args = zip(repeat(read_client), repeat(write_client), 
