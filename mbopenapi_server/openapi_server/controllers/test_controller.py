82ca40d34a3a17cc68136ad8a93a6be45149c6a3
artlands <lijie0416@gmail.com>
2020-04-29 13:01:44 -0500

    test

--- mbopenapi_server/openapi_server/controllers/test_controller.py
+++ mbopenapi_server/openapi_server/controllers/test_controller.py
@@ -123,7 +123,7 @@ def get_unified_metric(start, end, interval, value):  # noqa: E501
     # print(json.dumps(unified_metrics, indent=4))
     return
 
-start = "2020-04-06T12:00:00Z"
+start = "2020-04-05T12:00:00Z"
 end = "2020-04-07T12:00:00Z"
 interval = "1h"
 value = "max"
