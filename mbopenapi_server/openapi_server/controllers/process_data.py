2a23c78ce7fd207615807cc3b4d586bf3325026e
artlands <lijie0416@gmail.com>
2020-04-29 13:04:01 -0500

    test

--- mbopenapi_server/openapi_server/controllers/process_data.py
+++ mbopenapi_server/openapi_server/controllers/process_data.py
@@ -96,9 +96,9 @@ def process_node_data(node: str, node_data: dict, value: str, time_list: list) -
                     this_job_list = job_list[i-1]
             job_list.append(this_job_list)
 
-        # print("job_list: ", end = " ")
-        # print(len(job_list))
-        # print(json.dumps(job_list, indent=4))
+        print("job_list: ", end = " ")
+        print(len(job_list))
+        print(json.dumps(job_list, indent=4))
         
         
         job_set = list(set(job_list_temp))
