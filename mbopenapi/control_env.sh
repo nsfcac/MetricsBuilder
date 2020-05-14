de66ed4bb1f01ce1cf176e630dda0f6dc4c92a45
artlands <lijie0416@gmail.com>
2020-05-14 10:35:38 -0500

    activate virtual env in bash

---
+++ mbopenapi/control_env.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+activate(){
+    . ../env/bin/activate
+}
+
+if [ $# == 0 ]
+  then
+    echo "Please specify a argument: 0 to start MetricsBuilderAPI, other value to stop MetricsBuilderAPI."
+    exit 0
+fi
+
+if [ $1 == 0 ]
+then
+    echo "Start MetricsBuilderAPI ..."
+    nohup python3.6 -m openapi_server >> running.log &
+else
+    echo "Stop MetricsBuilderAPI ..."
+    kill -9 `ps -ef | grep openapi_server | grep -v grep | awk '{print $2}'`
+fi
+
+# gunicorn --bind 0.0.0.0:8080 wsgi:gunicorn_app
