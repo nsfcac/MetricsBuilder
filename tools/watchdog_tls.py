7dbc199b7028b26be43c8cdb1a91fc97b411eb7c
artlands <lijie0416@gmail.com>
2020-05-14 17:38:23 -0500

    test tls

--- tools/watchdog_tls.py
+++ tools/watchdog_tls.py
@@ -13,16 +13,9 @@ This message is sent from Python."""
 # Create a secure SSL context
 context = ssl.create_default_context()
 
-# Try to log in to server and send email
-try:
-    server = smtplib.SMTP(smtp_server,port)
-    server.ehlo() # Can be omitted
-    server.starttls(context=context) # Secure the connection
-    server.ehlo() # Can be omitted
+with smtplib.SMTP(smtp_server, port) as server:
+    server.ehlo()  # Can be omitted
+    server.starttls(context=context)
+    server.ehlo()  # Can be omitted
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message)
-except Exception as e:
-    # Print any error messages to stdout
-    print(e)
-finally:
-    server.quit()
