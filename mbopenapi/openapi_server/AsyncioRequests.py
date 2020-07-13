6fe9b94030b050ef0304a3549733310e946f55a0
artlands <lijie0416@gmail.com>
2020-07-12 19:56:33 -0500

    test AsynicoRequests

--- mbopenapi/openapi_server/AsyncioRequests.py
+++ mbopenapi/openapi_server/AsyncioRequests.py
@@ -29,7 +29,7 @@ class AsyncioRequests:
         try:
             resp = await client.query(sql)
             values = await resp["results"][0]["series"][0]
-            return await {"node": node, "data": values}
+            return {"node": node, "data": values}
         except:
             # logging.error(f"Error : Cannot fetch data from {node} : {sql}")
             return {"node": node, "data": {}}
