b578f99183922438a5c4658a50c036d9a459a642
artlands <lijie0416@gmail.com>
2020-07-14 14:43:52 -0500

    modify loop

--- mbopenapi/openapi_server/controllers/query_nodedata.py
+++ mbopenapi/openapi_server/controllers/query_nodedata.py
@@ -19,21 +19,22 @@ def query_nodedata(node: str, influx_cfg: dict, measurements: dict,
         sqls = generate_sqls(node, measurements, start, end, interval, value)
 
         # Query data
-        # loop  = asyncio.get_event_loop()
-        node_data = query_influx(influx_cfg, sqls)
-
+        loop = asyncio.get_event_loop()
+        node_data = query_influx(influx_cfg, sqls, loop)
+        loop.close()
+        
     except Exception as err:
         logging.error(f"query_nodedata error: {err}")
     return node_data
 
 
-def query_influx(influx_cfg: dict, sqls: list) -> list:
+def query_influx(influx_cfg: dict, sqls: list, loop) -> list:
     """
     Use NodeAsyncioRequests to query urls
     """
     data = []
     try:
-        request = NodeAsyncioRequests(influx_cfg['host'], influx_cfg['port'], influx_cfg['database'])
+        request = NodeAsyncioRequests(influx_cfg['host'], influx_cfg['port'], influx_cfg['database'], loop)
         data = request.bulk_fetch(sqls)
     except Exception as err:
         logging.error(f"query_nodedata : query_influx : {err}")
