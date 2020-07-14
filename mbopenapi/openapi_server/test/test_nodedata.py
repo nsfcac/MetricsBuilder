82657ffccec816e1eacf5cd2d7f6d5d5c064b3bc
artlands <lijie0416@gmail.com>
2020-07-13 22:28:44 -0500

    add asyncio data sample

--- mbopenapi/openapi_server/test/test_nodedata.py
+++ mbopenapi/openapi_server/test/test_nodedata.py
@@ -70,17 +70,18 @@ time_list = gen_epoch_timelist(start_time, end_time, interval)
 
 # # cores= multiprocessing.cpu_count()
 
-query_nodedata_args = zip(node_list, repeat(influx_cfg), repeat(measurements),
-                          repeat(start), repeat(end), repeat(interval), repeat(value))
+# query_nodedata_args = zip(node_list, repeat(influx_cfg), repeat(measurements),
+#                           repeat(start), repeat(end), repeat(interval), repeat(value))
 
-with multiprocessing.Pool() as pool:
-    # query data
-    results = pool.starmap(query_nodedata, query_nodedata_args)
-    # process data
-    process_nodedata_args = zip(results, repeat(time_list))
-    processed_results = pool.starmap(process_nodedata, process_nodedata_args)
-# # node = '10.101.2.35'
-# # nodedata = query_nodedata(node, influx_cfg, measurements, start, end, interval, value)
+# with multiprocessing.Pool() as pool:
+#     # query data
+#     results = pool.starmap(query_nodedata, query_nodedata_args)
+#     # process data
+#     process_nodedata_args = zip(results, repeat(time_list))
+#     processed_results = pool.starmap(process_nodedata, process_nodedata_args)
+
+node = '10.101.2.35'
+nodedata = query_nodedata(node, influx_cfg, measurements, start, end, interval, value)
 
 # # results = process_nodedata(nodedata)
-print(json.dumps(processed_results, indent=4))
+print(json.dumps(nodedata, indent=4))
