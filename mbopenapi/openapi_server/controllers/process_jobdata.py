e570eb6fc49ed66404c742c5eb757f79404ddcf1
artlands <lijie0416@gmail.com>
2020-07-14 12:16:54 -0500

    test query_jobdata

--- mbopenapi/openapi_server/controllers/process_jobdata.py
+++ mbopenapi/openapi_server/controllers/process_jobdata.py
@@ -7,3 +7,17 @@ def generate_jobset(processed_nodedata: dict) -> set:
         job_id = values['job_id']
         flatten = list(set([item for sublist in job_id for item in sublist]))
     return flatten
+
+
+def process_jobdata(jobdata: dict) -> dict:
+    jobdata = {}
+    job = jobdata["job"]
+    columns = jobdata["values"]["columns"]
+    values = jobdata["values"]["values"][0]
+
+    for i, column in enumerate(columns):
+        jobdata.update({
+            column: values[i]
+        })
+    
+    return {job: jobdata}
