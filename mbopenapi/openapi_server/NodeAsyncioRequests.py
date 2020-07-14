b8c9e8a42cc447a0400df9fdf5f004f814e08be4
artlands <lijie0416@gmail.com>
2020-07-14 13:46:06 -0500

    test mb

--- mbopenapi/openapi_server/NodeAsyncioRequests.py
+++ mbopenapi/openapi_server/NodeAsyncioRequests.py
@@ -3,7 +3,6 @@ import json
 import time
 import logging
 import asyncio
-import asyncio
 from aioinflux import InfluxDBClient
 
 
@@ -11,7 +10,7 @@ class NodeAsyncioRequests:
     """
     Node Asyncio requests to Influxdb
     """
-    import asyncio
+    # import asyncio
 
 
     def __init__(self, host: str, port: str, database: str):
@@ -19,7 +18,7 @@ class NodeAsyncioRequests:
         self.port = port
         self.database = database
         self.data = {}
-        self.loop = self.asyncio.get_event_loop()
+        self.loop = asyncio.get_event_loop()
     
 
     def __find_label_node(self, sql:str) -> str:
