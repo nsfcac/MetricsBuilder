75370fc89873986b1eb8810f126427a47d0a3f40
artlands <lijie0416@gmail.com>
2020-07-14 09:32:02 -0500

    test process_nodedata

--- mbopenapi/openapi_server/controllers/process_nodedata.py
+++ mbopenapi/openapi_server/controllers/process_nodedata.py
@@ -84,7 +84,7 @@ def aggregate_nodedata(node: str, organized: dict, time_list: list) -> dict:
             })
             labels_list = list(labels.keys())
             if len(labels_list) == 1:
-                if new_key == "job_list":
+                if measurement == "NodeJobs":
                     label_value = process_joblist(organized[measurement][labels_list[0]], time_list)
                 else:
                     label_value = organized[measurement][labels_list[0]]
