1ae75901b9e08212d7f9cc565b9808631e96448f
artlands <lijie0416@gmail.com>
2020-07-14 12:01:14 -0500

    test query_jobdata

---
+++ mbopenapi/openapi_server/JobAsyncioRequests.py
@@ -0,0 +1,69 @@
+import re
+import json
+import time
+import logging
+import asyncio
+import asyncio
+from aioinflux import InfluxDBClient
+
+
+class AsyncioRequests:
+    """
+    Asyncio requests to Influxdb
+    """
+    import asyncio
+
+
+    def __init__(self, host: str, port: str, database: str):
+        self.host = host
+        self.port = port
+        self.database = database
+        self.data = {}
+        self.loop = self.asyncio.get_event_loop()
+    
+
+    def __find_jobid(self, sql:str) -> str:
+        """
+        Parse job id from sql string
+        """
+        jobid = None
+        try:
+            # Parse lable and node id
+            jobid_pattern = "JobId='[\s\S]*'"
+            jobid = re.findall(jobid_pattern, sql)[0].split("=")[1][1:-1]
+        except Exception as err:
+            logging.error(f"Error : Cannot parse sql string: {sql} : {err}")
+        return jobid
+    
+
+    async def __fetch_json(self, sql: str, client: object) -> dict:
+        """
+        Get request wrapper to fetch json data from Influxdb
+        """
+        jobid = self.__find_label_node(sql)
+        json = {}
+        resp = {}
+        try:
+            resp = await client.query(sql)
+            # series = resp['results'][0].get('series', None)
+            # if series:
+            #     json = series[0]['values']
+            # else:
+            #     json = {}
+            #     # logging.warning(f"Warning : No {label} data from {node}")
+        except Exception as err:
+            logging.error(f"Error : Cannot fetch job data from {jobid}")
+        return {"job": jobid, "values": resp}
+
+
+    async def __requests(self, sqls: list) -> list:
+        async with InfluxDBClient(host = self.host, port = self.port, db = self.database) as client:
+            tasks = []
+            for i, sql in enumerate(sqls):
+                tasks.append(self.__fetch_json(sql=sql, client=client))
+            return await self.asyncio.gather(*tasks)
+
+
+    def bulk_fetch(self, sqls: list) -> list:
+        self.data =  self.loop.run_until_complete(self.__requests(sqls))
+        return self.data
