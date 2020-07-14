20eab3121bde200b1381f6f66d035c18d1b68af7
artlands <lijie0416@gmail.com>
2020-07-14 12:23:14 -0500

    test query_jobdata

--- mbopenapi/openapi_server/test/test_nodedata.py
+++ mbopenapi/openapi_server/test/test_nodedata.py
@@ -57,7 +57,7 @@ measurements = {
   ]
 }
 
-# node_list = parse_nodelist(nodelist_cfg)
+node_list = parse_nodelist(nodelist_cfg)
 
 start = "2020-07-12T12:00:00-05:00"
 end = "2020-07-12T18:00:00-05:00"
@@ -73,7 +73,7 @@ time_list = gen_epoch_timelist(start_time, end_time, interval)
 # # cores= multiprocessing.cpu_count()
 
 # node_list = ["10.101.1.1", "10.101.2.35", "10.101.1.3"]
-node_list = ['10.101.2.35']
+# node_list = ['10.101.2.35']
 query_nodedata_args = zip(node_list, repeat(influx_cfg), repeat(measurements),
                           repeat(start), repeat(end), repeat(interval), repeat(value))
 
