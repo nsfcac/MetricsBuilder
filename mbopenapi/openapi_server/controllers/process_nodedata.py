e94be4216712102d41fa8fa19877ebd4031906e4
artlands <lijie0416@gmail.com>
2020-07-13 19:04:47 -0500

    test process node data

--- mbopenapi/openapi_server/controllers/process_nodedata.py
+++ mbopenapi/openapi_server/controllers/process_nodedata.py
@@ -1,6 +1,6 @@
 def process_nodedata(nodedata: list) -> dict:
 
-    processed_data = {}
+    organized = {}
 
     for data in nodedata:
         # Get node - measurement - label names
@@ -13,24 +13,45 @@ def process_nodedata(nodedata: list) -> dict:
         flatten_values = [value[1] for value in values]
 
         # Build a dict
-        if measurement in processed_data:
-            processed_data[measurement].update({
+        if measurement in organized:
+            organized[measurement].update({
                 label: flatten_values
             })
         else:
-            processed_data.update({
+            organized.update({
                 measurement: {
                     label: flatten_values
                 }
             })
-    # json_data = {
-    #         "memory_usage": memory_usage,
-    #         "cpu_usage": cpu_usage,
-    #         "power_usage": power_usage,
-    #         "fan_speed": fan_speed,
-    #         "cpu_inl_temp": cpu_inl_temp,
-    #         "job_list": job_list,
-    #         "job_set": job_set
-    #     }
 
-    return processed_data
+    # Mapping data points
+    # To do: make it automatically
+    memory_usage = organized["MemUsage"]["UGE"]
+    cpu_usage = organized["CPUUsage"]["UGE"]
+    power_usage = organized["Power"]["NodePower"]
+
+    fan_1 = organized["FanSensor"]["FAN_1"]
+    fan_2 = organized["FanSensor"]["FAN_2"]
+    fan_3 = organized["FanSensor"]["FAN_3"]
+    fan_4 = organized["FanSensor"]["FAN_4"]
+
+    fan_speed = [ [fan_1[i], fan_2[i], fan_3[i], fan_4[i]] for i in range(len(fan_1)) ]
+
+    cpu_1_temp = organized["TempSensor"]["CPU1 Temp"]
+    cpu_2_temp = organized["TempSensor"]["CPU2 Temp"]
+    inlet_temp = organized["TempSensor"]["Inlet Temp"]
+
+    cpu_inl_temp = [ [cpu_1_temp[i], cpu_2_temp[i], inlet_temp[i]] for in range(len(cpu_1_temp)) ]
+
+    job_list = organized["NodeJobs"]["JobList"]
+
+    json_data = {
+        "memory_usage": memory_usage,
+        "cpu_usage": cpu_usage,
+        "power_usage": power_usage,
+        "fan_speed": fan_speed,
+        "cpu_inl_temp": cpu_inl_temp,
+        "job_list": job_list,
+    }
+
+    return json_data
