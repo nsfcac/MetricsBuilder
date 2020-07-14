c393f5830830ff15cf10080550fb71c6bf4e255c
artlands <lijie0416@gmail.com>
2020-07-14 12:58:52 -0500

    test query_jobdata

--- mbopenapi/openapi_server/controllers/process_jobdata.py
+++ mbopenapi/openapi_server/controllers/process_jobdata.py
@@ -10,14 +10,45 @@ def generate_jobset(processed_nodedata: dict) -> set:
 
 
 def process_jobdata(jobdata: dict) -> dict:
+    """
+    Process job data based on the mapping relationship
+    """
     processed_jobdata = {}
+    organized_jobdata = {}
+    
+    job_array = False
+
+    mapping = {
+        "StartTime": "start_time",
+        "SubmitTime": "submit_time",
+        "JobName": "job_name",
+        "User": "user_name",
+        "node_list": "NodeList",
+        "total_nodes": "TotalNodes",
+        "cpu_cores": "CPUCores",
+    }
+
+    organized_jobdata = {}
     job = jobdata["job"]
     columns = jobdata["values"]["columns"]
     values = jobdata["values"]["values"][0]
 
     for i, column in enumerate(columns):
-        processed_jobdata.update({
+        organized_jobdata.update({
             column: values[i]
         })
+
+    for key, value in mapping.items():
+        processed_jobdata.update({
+            value: organized_jobdata[key]
+        })
+    
+    if "." in organized_jobdata["JobId"]:
+        job_array = False
+
+    processed_jobdata.update({
+        "finish_time": None,
+        "job_array": job_array
+    })
     
     return {job: processed_jobdata}
