15fdd338133c9ad4127d34e8b0bdc103c277a69c
artlands <lijie0416@gmail.com>
2020-07-14 13:13:09 -0500

    test query_jobdata

--- mbopenapi/openapi_server/controllers/process_jobdata.py
+++ mbopenapi/openapi_server/controllers/process_jobdata.py
@@ -1,11 +1,17 @@
+import logging
+
+
 def generate_jobset(processed_nodedata: dict) -> set:
     """
     Generate job set from 'job_id'
     """
     flatten = []
-    for node, values in processed_nodedata.items():
-        job_id = values['job_id']
-        flatten = list(set([item for sublist in job_id for item in sublist]))
+    try:
+        for node, values in processed_nodedata.items():
+            job_id = values['job_id']
+            flatten = list(set([item for sublist in job_id for item in sublist]))
+    except Exception as err:
+        logging.error(f"generate_jobset : {err}")
     return flatten
 
 
@@ -13,34 +19,33 @@ def process_jobdata(jobdata: dict) -> dict:
     """
     Process job data based on the mapping relationship
     """
-    processed_jobdata = {}
-    organized_jobdata = {}
-
-    job_array = False
-
-    mapping = {
-        "StartTime": "start_time",
-        "SubmitTime": "submit_time",
-        "JobName": "job_name",
-        "User": "user_name",
-        "NodeList": "node_list",
-        "TotalNodes": "total_nodes",
-        "CPUCores": "cpu_cores"
-    }
-
-    organized_jobdata = {}
     job = jobdata["job"]
-    columns = jobdata["values"]["columns"]
-    values = jobdata["values"]["values"][0]
+    processed_jobdata = {}
+    job_array = False
+    try:
+        mapping = {
+            "StartTime": "start_time",
+            "SubmitTime": "submit_time",
+            "JobName": "job_name",
+            "User": "user_name",
+            "NodeList": "node_list",
+            "TotalNodes": "total_nodes",
+            "CPUCores": "cpu_cores"
+        }
 
-    for i, column in enumerate(columns):
-        if column != "time" and column != "JobId":
-            organized_jobdata.update({
-                mapping[column]: values[i]
-            })
-        else:
-            organized_jobdata.update({
-                "finish_time": None
-            })
+        columns = jobdata["values"]["columns"]
+        values = jobdata["values"]["values"][0]
+
+        for i, column in enumerate(columns):
+            if column != "time" and column != "JobId":
+                processed_jobdata.update({
+                    mapping[column]: values[i]
+                })
+            else:
+                processed_jobdata.update({
+                    "finish_time": None
+                })
+    except Exception as err:
+        logging.error(f"process_jobdata : {job} : {err}")
     
-    return {job: organized_jobdata}
+    return {job: processed_jobdata}
