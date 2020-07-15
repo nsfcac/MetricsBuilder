65fe8c0527b8d58e572c5b82b7effeb02ef790f7
artlands <lijie0416@gmail.com>
2020-07-14 20:17:17 -0500

    test query_nodedata

--- mbopenapi/openapi_server/test/test_nodedata.py
+++ mbopenapi/openapi_server/test/test_nodedata.py
@@ -62,14 +62,16 @@ client = InfluxDBClient(host=influx_cfg['host'], port=influx_cfg['port'], databa
 # node_list = parse_nodelist(nodelist_cfg)
 
 start = "2020-07-12T12:00:00-05:00"
-end = "2020-07-12T18:00:00-05:00"
-interval = "5m"
+end = "2020-07-12T14:00:00-05:00"
+interval = "30m"
 value_type = "max"
 
 start_time = util.deserialize_datetime(start)
 end_time = util.deserialize_datetime(end)
 
 time_list = gen_timelist(start_time, end_time, interval)
+
+print(time_list)
 # print(json.dumps(time_list, indent = 4))
 
 # # cores= multiprocessing.cpu_count()
