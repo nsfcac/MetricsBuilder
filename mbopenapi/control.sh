d6f8dbe92c5032684d1fbcba232b26a6efa524cb
artlands <lijie0416@gmail.com>
2020-07-20 00:04:42 -0500

    modify control

--- mbopenapi/control.sh
+++ mbopenapi/control.sh
@@ -4,23 +4,21 @@ readonly sourceFile="/home/username/TestMB/MetricsBuilder/env/bin/activate"
 
 source ${sourceFile}
 
-if [ $# == 0 ]
-  then
-    echo "Please specify a argument: 0 to start MetricsBuilderAPI, other value to stop MetricsBuilderAPI."
-    exit 0
-fi
+/home/username/TestMB/MetricsBuilder/env/bin/python3 -m openapi_server
 
-if [ $1 == 0 ]
-then
-    # activate
-    echo "Start MetricsBuilderAPI ..."
-    cd /home/username/TestMB/MetricsBuilder/mbopenapi/
-    nohup python3 -m openapi_server >> running.log &
-else
-    echo "Stop MetricsBuilderAPI ..."
-    kill -9 `ps -ef | grep openapi_server | grep -v grep | awk '{print $2}'`
-fi
+# if [ $# == 0 ]
+#   then
+#     echo "Please specify a argument: 0 to start MetricsBuilderAPI, other value to stop MetricsBuilderAPI."
+#     exit 0
+# fi
 
-# gunicorn --bind 0.0.0.0:8080 wsgi:gunicorn_app
-
-# 0 */1 * * * /home/username/openapi_ctrl.sh
+# if [ $1 == 0 ]
+# then
+#     # activate
+#     echo "Start MetricsBuilderAPI ..."
+#     cd /home/username/TestMB/MetricsBuilder/mbopenapi/
+#     nohup python3 -m openapi_server >> running.log &
+# else
+#     echo "Stop MetricsBuilderAPI ..."
+#     kill -9 `ps -ef | grep openapi_server | grep -v grep | awk '{print $2}'`
+# fi
