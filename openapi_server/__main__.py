cc582c7bc22986b86c9d441f0bcd288bb6154aa1
artlands <lijie0416@gmail.com>
2020-07-28 10:06:58 -0500

    change main

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -7,19 +7,13 @@ import ssl
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain("/home/username/SSL_Certificate/influx_ttu_edu_cert.cer", "/home/username/SSL_Certificate/server.key")
 
-
-def main():
-    app = connexion.App(__name__, specification_dir='./openapi/', server='gevent')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'MetricsBuilder API'},
-                pythonic_params=True)
-
-    CORS(app.app)
-    
-    app.run(port=8080, ssl_context=context)
-    # app.run()
+app = connexion.App(__name__, specification_dir='./openapi/', server='gevent')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml',
+            arguments={'title': 'MetricsBuilder API'},
+            pythonic_params=True)
+CORS(app.app)
 
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8080, ssl_context=context)
