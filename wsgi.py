4386bbeab468797206b457cbb18c78f623d4a89e
artlands <lijie0416@gmail.com>
2020-07-28 17:13:34 -0500

    test apache

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 import sys
-# import site
-
-# site.addsitedir('/var/www/MetricsBuilder/env/lib/python3.6/site-packages')
 
 sys.path.append('/var/www/MetricsBuilder')
 
 from openapi_server.__main__ import app as application
+
+if __name__ == "__main__":
+    application.run()
