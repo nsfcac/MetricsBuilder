0d1c9ed8bbee1cbea7c48a62c2d31b3c190d785c
artlands <lijie0416@gmail.com>
2020-07-28 10:25:18 -0500

    test gunicorn

--- control.sh
+++ control.sh
@@ -6,7 +6,9 @@ source ${sourceFile}
 
 # python3 -m openapi_server
 
-gunicorn --certfile='/home/username/SSL_Certificate/influx_ttu_edu_cert.cer' --keyfile='/home/username/SSL_Certificate/server.key' -b 0.0.0.0:8080 'openapi_server.__main__:app'
+gunicorn --certfile='/home/username/SSL_Certificate/influx_ttu_edu_cert.cer' --keyfile='/home/username/SSL_Certificate/server.key' --worker-class=gevent --worker-connections=1000 --workers=5 --timeout 120 -b 0.0.0.0:8080 'openapi_server.__main__:app' --limit-request-line 0
+
+# gunicorn --certfile='/home/username/SSL_Certificate/influx_ttu_edu_cert.cer' --keyfile='/home/username/SSL_Certificate/server.key' -b 0.0.0.0:8080 'openapi_server.__main__:app'
 
 # Jul 28 02:47:19 influx control.sh[26221]: File "/usr/lib64/python3.6/ssl.py", line 365, in wrap_socket
 # Jul 28 02:47:19 influx control.sh[26221]: _context=self, _session=session)
@@ -20,3 +22,4 @@ gunicorn --certfile='/home/username/SSL_Certificate/influx_ttu_edu_cert.cer' --k
 # Jul 28 02:47:19 influx control.sh[26221]: 2020-07-28T07:47:19Z <Greenlet at 0x7f4388a05b48: _handle_and_close_when_done(<bound method StreamServer.wrap_socket_and_handle , <bound me
 
 # gunicorn --certfile='/home/username/SSL_Certificate/influx_ttu_edu_cert.cer' --keyfile='/home/username/SSL_Certificate/server.key' -b 0.0.0.0:8080 'openapi_server.__main__:app' --limit-request-line 0
+# gunicorn --certfile='/home/username/SSL_Certificate/influx_ttu_edu_cert.cer' --keyfile='/home/username/SSL_Certificate/server.key' --worker-class=gevent --worker-connections=1000 --workers=5 --timeout 120 -b 0.0.0.0:8080 'openapi_server.__main__:app' --limit-request-line 0
