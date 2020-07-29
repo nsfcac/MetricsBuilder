4f2221feebeec86f61b49772071e7f24b478fedf
artlands <lijie0416@gmail.com>
2020-07-28 19:00:56 -0500

    test apache

--- gunicorn.sh
+++ gunicorn.sh
@@ -4,7 +4,7 @@ readonly sourceFile="/home/username/MetricsBuilder/env/bin/activate"
 
 source ${sourceFile}
 
-gunicorn --certfile='/home/username/SSL_Certificate/influx_ttu_edu_cert.cer' --keyfile='/home/username/SSL_Certificate/server.key' --workers=5 --timeout 120 -b 0.0.0.0:8080 'openapi_server.__main__:app' --limit-request-line 0
+gunicorn --certfile='/home/username/SSL_Certificate/influx_ttu_edu_cert.cer' --keyfile='/home/username/SSL_Certificate/server.key' --workers=5 --timeout 120 -b 0.0.0.0:8080 'openapi_server.__main__:app' --log-file error_logs.log --log-level error --name MetricsBuilderAPI
 
 # gunicorn --certfile='/home/username/SSL_Certificate/influx_ttu_edu_cert.cer' --keyfile='/home/username/SSL_Certificate/server.key' -b 0.0.0.0:8080 'openapi_server.__main__:app'
 
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.append('/var/www/MetricsBuilder')
+sys.path.append('/home/username/MetricsBuilder')
 
 from openapi_server.__main__ import app as application
 
