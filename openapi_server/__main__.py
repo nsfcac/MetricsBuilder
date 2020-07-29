52297c49728c719f845c4517bf0d8ccd6f2cccbb
artlands <lijie0416@gmail.com>
2020-07-29 12:44:23 -0500

    uge gevent

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -7,8 +7,7 @@ import ssl
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain("/home/username/SSL_Certificate/influx_ttu_edu_cert.cer", "/home/username/SSL_Certificate/server.key")
 
-# app = connexion.App(__name__, specification_dir='./openapi/', server='gevent')
-app = connexion.App(__name__, specification_dir='./openapi/')
+app = connexion.App(__name__, specification_dir='./openapi/', server='gevent')
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('openapi.yaml',
             arguments={'title': 'MetricsBuilder API'},
