ef0886300b06c9b6f2fb3a065180f46ac7a4751f
artlands <lijie0416@gmail.com>
2020-08-06 23:39:18 -0500

    update process job data, remove set finish time to None

--- openapi_server/controllers/process_jobdata.py
+++ openapi_server/controllers/process_jobdata.py
@@ -13,7 +13,7 @@ def process_jobdata(jobdata: list) -> dict:
             mapping = {
                 "StartTime": "start_time",
                 "SubmitTime": "submit_time",
-                # "FinishTime": "finish_time",
+                "FinishTime": "finish_time",
                 "JobName": "job_name",
                 "User": "user_name",
                 "NodeList": "node_list",
@@ -24,12 +24,17 @@ def process_jobdata(jobdata: list) -> dict:
             values = data["values"][0]
 
             # This is a temporary solution for getting finish time
-            finish_time = processed_jobdata[job].get("FinishTime", None)
+            if "FinishTime" not in values:
+                finish_time = None
+            else:
+                finish_time = values["FinishTime"]
+                
             processed_jobdata.update({
                 job: {
                     "finish_time": finish_time
                 }
             })
+
             
             for key, value in values.items():
                 if key != "time" and key != "JobId":
