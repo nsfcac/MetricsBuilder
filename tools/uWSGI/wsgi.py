d61f22cdc2f7dbaebe01bda6b773c9674eda4b84
artlands <lijie0416@gmail.com>
2020-08-08 23:38:21 -0500

    test wsgi

--- tools/uWSGI/wsgi.py
+++ tools/uWSGI/wsgi.py
@@ -2,12 +2,7 @@ import sys
 
 sys.path.append('/home/username/MetricsBuilder')
 
-import ssl
-
-context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-context.load_cert_chain("/home/username/SSL_Certificate/influx_ttu_edu_cert.cer", "/home/username/SSL_Certificate/server.key")
-
 from openapi_server.__main__ import app as application
 
 if __name__ == "__main__":
-    application.run(port=8080, ssl_context=context)
+    application.run(port=8080)
