b96ecc2eaade4af875ee2f8347f0c38b6a83d781
artlands <lijie0416@gmail.com>
2020-08-10 00:01:40 -0500

    test gevent

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -16,8 +16,9 @@ app.add_api('openapi.yaml',
 CORS(app.app)
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('localhost', 8080), app, keyfile="/home/lijie/SSL_Certificate/server.key", 
+    http_server = WSGIServer(('', 8080), app, keyfile="/home/lijie/SSL_Certificate/server.key", 
                               certfile="/home/lijie/SSL_Certificate/influx_ttu_edu_cert.cer")
-    http_server.serve_forever()
+    http_server.start()
+    # http_server.serve_forever()
     # app.run(port=8080, ssl_context=context)
     # app.run(port=8080)
